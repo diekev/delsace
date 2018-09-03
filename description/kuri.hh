718efbd8de530b180b745c43d0c35824f837741f
Kévin Dietrich <kevin.dietrich@mailoo.org>
2018-09-03 22:24:28 +0200

    Fichier de définitions des mot-clés kuri pour C++.

---
+++ description/kuri.hh
@@ -0,0 +1,53 @@
+#pragma once
+
+using entier8 = char;
+using entier16 = short;
+using entier32 = int;
+using entier64 = long;
+
+using e8 = char;
+using e16 = short;
+using e32 = int;
+using e64 = long;
+
+using entier8_ns = unsigned char;
+using entier16_ns = unsigned short;
+using entier32_ns = unsigned int;
+using entier64_ns = unsigned long;
+
+using e8_ns = unsigned char;
+using e16_ns = unsigned short;
+using e32_ns = unsigned int;
+using e64_ns = unsigned long;
+
+using decimal32 = float;
+using decimal64 = double;
+
+using d32 = float;
+using d64 = double;
+
+using booleen = bool;
+
+using vide = void;
+
+#define pointeur void*
+
+#define nul nullptr
+
+#define vrai true
+
+#define faux false
+
+#define si if
+
+#define sinon else
+
+#define arrete break
+
+#define retourne return
+
+#define constant const
+
+#define gabarit template
+
+#define soit auto
