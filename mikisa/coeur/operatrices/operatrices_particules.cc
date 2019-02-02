d8f8b2c97a7e61091c2ed0c58f5da1d1ab066ef7
Kévin Dietrich <kevin.dietrich@mailoo.org>
2019-02-02 08:02:18 +0100

    Ajout d'un opératrice de dispersion de point utilisant un algorithme de tirage de flèche.

--- mikisa/coeur/operatrices/operatrices_particules.cc
+++ mikisa/coeur/operatrices/operatrices_particules.cc
@@ -24,7 +24,10 @@
 
 #include "operatrices_particules.h"
 
+#include <map>
 #include <random>
+#include <sstream>
+#include <stack>
 
 #include "../corps/corps.h"
 
@@ -99,7 +102,21 @@ public:
 /* ************************************************************************** */
 
 struct Triangle {
-	dls::math::vec3f v0{}, v1{}, v2{};
+	dls::math::vec3f v0 = dls::math::vec3f(0.0f, 0.0f, 0.0f);
+	dls::math::vec3f v1 = dls::math::vec3f(0.0f, 0.0f, 0.0f);
+	dls::math::vec3f v2 = dls::math::vec3f(0.0f, 0.0f, 0.0f);
+	float aire = 0.0f;
+	Triangle *precedent = nullptr, *suivant = nullptr;
+
+	Triangle() = default;
+
+	Triangle(const dls::math::vec3f &v_0, const dls::math::vec3f &v_1, const dls::math::vec3f &v_2)
+		: Triangle()
+	{
+		v0 = v_0;
+		v1 = v_1;
+		v2 = v_2;
+	}
 };
 
 std::vector<Triangle> convertis_maillage_triangles(Corps const *corps_entree)
@@ -264,10 +281,518 @@ public:
 
 /* ************************************************************************** */
 
+/**
+ * "Dart Throwing on Surfaces" (Cline et al. 2009)
+ * http://peterwonka.net/Publications/pdfs/2009.EGSR.Cline.PoissonSamplingOnSurfaces.pdf
+ */
+
+/* La densité de l'arrangement de cercles ayant la plus grande densité, selon
+ * Lagrange. C'est-à-dire le pourcentage d'aire qu'occuperait un arrangement de
+ * cercle étant le plus compacte. */
+static constexpr auto DENSITE_CERCLE = 0.9068996821171089f;
+
+static constexpr auto NOMBRE_BOITE = 64;
+
+/* ************************************************************************** */
+
+struct HachageSpatial {
+	std::unordered_map<std::size_t, std::vector<dls::math::vec3f>> m_tableau{};
+
+	/**
+	 * La taille maximum recommandée par la publication de Cline et al. est de
+	 * 20 000. Cependant, les fonctions de hachage marche mieux quand la taille
+	 * est un nombre premier ("Introduction to Algorithms", ISBN 0-262-03141-8),
+	 * donc nous prenons le nombre premier le plus proche de 20 000.
+	 */
+	static constexpr auto TAILLE_MAX = 19997;
+
+	/**
+	 * Fonction de hachage repris de "Optimized Spatial Hashing for Collision
+	 * Detection of Deformable Objects"
+	 * http://www.beosil.com/download/CollisionDetectionHashing_VMV03.pdf
+	 *
+	 * Pour calculer l'empreinte d'une position, nous considérons la partie
+	 * entière de celle-ci. Par exemple, le vecteur position <0.1, 0.2, 0.3>
+	 * deviendra <0, 0, 0> ; de même pour le vecteur <0.4, 0.5, 0.6>. Ainsi,
+	 * toutes les positions se trouvant entre <0, 0, 0> et
+	 * <0.99.., 0.99.., 0.99..> seront dans la même alvéole.
+	 */
+	std::size_t fonction_empreinte(const dls::math::vec3f &position);
+
+	/**
+	 * Ajoute la posistion spécifiée dans le vecteur des positions ayant la même
+	 * empreinte que celle-ci.
+	 */
+	void ajoute(const dls::math::vec3f &position);
+
+	/**
+	 * Retourne un vecteur contenant les positions ayant la même empreinte que
+	 * la position passée en paramètre.
+	 */
+	const std::vector<dls::math::vec3f> &particules(const dls::math::vec3f &position);
+
+	/**
+	 * Retourne le nombre d'alvéoles présentes dans la table de hachage.
+	 */
+	size_t taille() const;
+};
+
+std::size_t HachageSpatial::fonction_empreinte(const dls::math::vec3f &position)
+{
+	return static_cast<std::size_t>(
+				static_cast<int>(position.x) * 73856093
+				^ static_cast<int>(position.y) * 19349663
+				^ static_cast<int>(position.z) * 83492791) % TAILLE_MAX;
+}
+
+void HachageSpatial::ajoute(const dls::math::vec3f &position)
+{
+	auto const empreinte = fonction_empreinte(position);
+	m_tableau[empreinte].push_back(position);
+}
+
+const std::vector<dls::math::vec3f> &HachageSpatial::particules(const dls::math::vec3f &position)
+{
+	auto const empreinte = fonction_empreinte(position);
+	return m_tableau[empreinte];
+}
+
+size_t HachageSpatial::taille() const
+{
+	return m_tableau.size();
+}
+
+/* ************************************************************************** */
+
+static float calcule_aire(const dls::math::vec3f &v0, const dls::math::vec3f &v1, const dls::math::vec3f &v2)
+{
+	auto const c1 = v1 - v0;
+	auto const c2 = v2 - v0;
+
+	return longueur(produit_croix(c1, c2)) * 0.5f;
+}
+
+static float calcule_aire(const Triangle &triangle)
+{
+	return calcule_aire(triangle.v0, triangle.v1, triangle.v2);
+}
+
+class ListeTriangle {
+	Triangle *m_premier_triangle = nullptr;
+	Triangle *m_dernier_triangle = nullptr;
+
+public:
+	ListeTriangle() = default;
+	~ListeTriangle() = default;
+
+	Triangle *ajoute(const dls::math::vec3f &v0, const dls::math::vec3f &v1, const dls::math::vec3f &v2)
+	{
+		Triangle *triangle = new Triangle(v0, v1, v2);
+		triangle->aire = calcule_aire(*triangle);
+		triangle->precedent = nullptr;
+		triangle->suivant = nullptr;
+
+		if (m_premier_triangle == nullptr) {
+			m_premier_triangle = triangle;
+		}
+		else {
+			triangle->precedent = m_dernier_triangle;
+			m_dernier_triangle->suivant = triangle;
+		}
+
+		m_dernier_triangle = triangle;
+
+		return triangle;
+	}
+
+	void enleve(Triangle *triangle)
+	{
+		if (triangle->precedent) {
+			triangle->precedent->suivant = triangle->suivant;
+		}
+
+		if (triangle->suivant) {
+			triangle->suivant->precedent = triangle->precedent;
+		}
+
+		if (triangle == m_premier_triangle) {
+			m_premier_triangle = triangle->suivant;
+
+			if (m_premier_triangle) {
+				m_premier_triangle->precedent = nullptr;
+			}
+		}
+
+		if (triangle == m_dernier_triangle) {
+			m_dernier_triangle = triangle->precedent;
+
+			if (m_dernier_triangle) {
+				m_dernier_triangle->precedent = nullptr;
+			}
+		}
+
+		delete triangle;
+	}
+
+	Triangle *premier_triangle()
+	{
+		return m_premier_triangle;
+	}
+
+	bool vide() const
+	{
+		return m_premier_triangle == nullptr;
+	}
+};
+
+struct BoiteTriangle {
+	float aire_minimum = std::numeric_limits<float>::max();
+	float aire_maximum = 0.0f; // = 2 * aire_minimum
+	float aire_totale = 0.0f;
+	float pad{};
+
+	ListeTriangle triangles{};
+};
+
+void ajoute_triangle_boite(BoiteTriangle *boite, const dls::math::vec3f &v0, const dls::math::vec3f &v1, const dls::math::vec3f &v2)
+{
+	auto triangle = boite->triangles.ajoute(v0, v1, v2);
+	boite->aire_minimum = std::min(boite->aire_minimum, triangle->aire);
+	boite->aire_maximum = 2 * boite->aire_minimum;
+	boite->aire_totale += triangle->aire;
+}
+
+bool verifie_distance_minimal(HachageSpatial &hachage_spatial, const dls::math::vec3f &point, float distance)
+{
+	auto const points = hachage_spatial.particules(point);
+
+	for (auto p = 0ul; p < points.size(); ++p) {
+		if (longueur(point - points[p]) < distance) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool triangle_couvert(const Triangle &triangle, HachageSpatial &hachage_spatial, const float radius)
+{
+	auto const &v0 = triangle.v0;
+	auto const &v1 = triangle.v1;
+	auto const &v2 = triangle.v2;
+
+	auto const centre_triangle = (v0 + v1 + v2) / 3.0f;
+	auto const points = hachage_spatial.particules(centre_triangle);
+
+	for (auto p = 0ul; p < points.size(); ++p) {
+		if (longueur(v0 - points[p]) <= radius
+			&& longueur(v1 - points[p]) <= radius
+			&& longueur(v2 - points[p]) <= radius)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+class OperatriceTirageFleche : public OperatriceCorps {
+public:
+	static constexpr auto NOM = "Tirage de flèche";
+	static constexpr auto AIDE =
+			"Crée des points sur une surface en l'échantillonnant par tirage de flèche.";
+
+	OperatriceTirageFleche(Graphe &graphe_parent, Noeud *noeud)
+		: OperatriceCorps(graphe_parent, noeud)
+	{
+		entrees(1);
+		sorties(1);
+	}
+
+	const char *chemin_entreface() const override
+	{
+		return "entreface/operatrice_tirage_fleche.jo";
+	}
+
+	int type_entree(int) const override
+	{
+		return OPERATRICE_CORPS;
+	}
+
+	int type_sortie(int) const override
+	{
+		return OPERATRICE_CORPS;
+	}
+
+	const char *nom_classe() const override
+	{
+		return NOM;
+	}
+
+	const char *texte_aide() const override
+	{
+		return AIDE;
+	}
+
+	int execute(const Rectangle &rectangle, const int temps) override
+	{
+		m_corps.reinitialise();
+
+		auto corps_maillage = entree(0)->requiers_corps(rectangle, temps);
+
+		if (corps_maillage == nullptr) {
+			this->ajoute_avertissement("Il n'y pas de corps en entrée !");
+			return EXECUTION_ECHOUEE;
+		}
+
+		auto const prims_maillage = corps_maillage->prims();
+
+		if (prims_maillage->taille() == 0) {
+			this->ajoute_avertissement("Il n'y pas de primitives dans le corps d'entrée !");
+			return EXECUTION_ECHOUEE;
+		}
+
+		/* Convertis le maillage en triangles. */
+		auto const points = corps_maillage->points();
+
+		auto aire_minimum = std::numeric_limits<float>::max();
+		auto aire_maximum = 0.0f;
+		auto aire_totale = 0.0f;
+
+		/* Calcule les informations sur les aires. */
+		for (auto prim : prims_maillage->prims()) {
+			if (prim->type_prim() != type_primitive::POLYGONE) {
+				continue;
+			}
+
+			auto polygone = dynamic_cast<Polygone *>(prim);
+
+			if (polygone->type != type_polygone::FERME) {
+				continue;
+			}
+
+			for (size_t i = 2; i < polygone->nombre_sommets(); ++i) {
+				auto const v0 = points->point(polygone->index_point(0));
+				auto const v1 = points->point(polygone->index_point(i - 1));
+				auto const v2 = points->point(polygone->index_point(i));
+
+				auto aire = calcule_aire(v0, v1, v2);
+				aire_minimum = std::min(aire_minimum, aire);
+				aire_maximum = std::max(aire_maximum, aire);
+				aire_totale += aire;
+			}
+		}
+
+		/* Place les triangles dans les boites. */
+		BoiteTriangle boites[NOMBRE_BOITE];
+
+		for (auto prim : prims_maillage->prims()) {
+			if (prim->type_prim() != type_primitive::POLYGONE) {
+				continue;
+			}
+
+			auto polygone = dynamic_cast<Polygone *>(prim);
+
+			if (polygone->type != type_polygone::FERME) {
+				continue;
+			}
+
+			for (size_t i = 2; i < polygone->nombre_sommets(); ++i) {
+				auto const v0 = points->point(polygone->index_point(0));
+				auto const v1 = points->point(polygone->index_point(i - 1));
+				auto const v2 = points->point(polygone->index_point(i));
+
+				auto aire = calcule_aire(v0, v1, v2);
+
+				auto const index_boite = static_cast<int>(std::log2(aire_maximum / aire));
+
+				if (index_boite < 0 || index_boite >= 64) {
+					std::stringstream ss;
+					ss << "Erreur lors de la génération de l'index d'une boîte !";
+					ss << "\n   Index : " << index_boite;
+					ss << "\n   Aire triangle : " << aire;
+					ss << "\n   Aire totale : " << aire_maximum;
+					this->ajoute_avertissement(ss.str());
+					continue;
+				}
+
+				ajoute_triangle_boite(&boites[index_boite], v0, v1, v2);
+			}
+		}
+
+		/* Ne considère que les triangles dont l'aire est supérieure à ce seuil. */
+		auto const seuil_aire = aire_minimum / 10000.0f;
+		auto const distance = evalue_decimal("distance");
+
+		/* Calcule le nombre maximum de point. */
+		auto const aire_cercle = static_cast<float>(M_PI) * (distance * 0.5f) * (distance * 0.5f);
+		auto const nombre_points = static_cast<size_t>((aire_totale * DENSITE_CERCLE) / aire_cercle);
+		std::cerr << "Nombre points prédits : " << nombre_points << '\n';
+
+		auto points_nuage = m_corps.points();
+		points_nuage->reserve(nombre_points);
+
+		auto const graine = evalue_entier("graine");
+		std::mt19937 rng(graine);
+		std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+
+		HachageSpatial hachage_spatial;
+
+		/* Tant qu'il reste des triangles à remplir... */
+		while (true) {
+			/* Choisis une boîte avec une probabilité proportionnelle à l'aire
+			 * total des fragments de la boîte. À FAIRE. */
+			BoiteTriangle *boite = nullptr;
+			auto boite_trouvee = false;
+//			auto index_boite = 0;
+
+			for (auto i = 0; i < NOMBRE_BOITE; ++i) {
+				if (boites[i].triangles.vide()) {
+					continue;
+				}
+
+				boite = &boites[i];
+				boite_trouvee = true;
+//				index_boite = i;
+				break;
+
+//				auto const probabilite_boite = boite->aire_totale / aire_totale;
+
+//				if (dist(rng) <= probabilite_boite) {
+//					boite_trouvee = true;
+//					break;
+//				}
+			}
+
+			/* Toutes les boites sont vides, arrêt de l'algorithme. */
+			if (!boite_trouvee) {
+				break;
+			}
+
+			/* Sélectionne un triangle proportionellement à son aire. */
+			Triangle *triangle = boite->triangles.premier_triangle();
+//			bool triangle_trouve = false;
+
+//			for (auto tri : boite->triangles.triangles()) {
+//				if (tri->jete) {
+//					continue;
+//				}
+
+//				triangle = tri;
+//				triangle_trouve = true;
+//				break;
+
+////				auto const probabilite_triangle = tri.aire / boite->aire_maximum;
+
+////				if (dist(rng) <= probabilite_triangle) {
+////					triangle = &tri;
+////					triangle_trouve = true;
+////					break;
+////				}
+//			}
+
+//			if (!triangle_trouve) {
+//				std::cerr << "Ne trouve pas de triangles !\n";
+//				std::cerr << "Boîte : " << index_boite << '\n';
+//				std::cerr << "Taille : " << boite->triangles.taille() << '\n';
+//				continue;
+//			}
+
+			/* Choisis un point aléatoire p sur le triangle en prenant une
+			 * coordonnée barycentrique aléatoire. */
+			auto const v0 = triangle->v0;
+			auto const v1 = triangle->v1;
+			auto const v2 = triangle->v2;
+			auto const e0 = v1 - v0;
+			auto const e1 = v2 - v0;
+
+			auto r = dist(rng);
+			auto s = dist(rng);
+
+			if (r + s >= 1.0f) {
+				r = 1.0f - r;
+				s = 1.0f - s;
+			}
+
+			auto point = v0 + r * e0 + s * e1;
+
+			/* Vérifie que le point respecte la condition de distance minimal */
+			auto ok = verifie_distance_minimal(hachage_spatial, point, distance);
+
+			if (ok) {
+				hachage_spatial.ajoute(point);
+				m_corps.ajoute_point(point.x, point.y, point.z);
+			}
+
+			/* Vérifie si le triangle est complétement couvert par un point de
+			 * l'ensemble. */
+			auto couvert = triangle_couvert(*triangle, hachage_spatial, distance);
+
+			if (couvert) {
+				/* Si couvert, jète le triangle. */
+				boite->aire_totale -= triangle->aire;
+				boite->triangles.enleve(triangle);
+			}
+			else {
+				/* Sinon, coupe le triangle en petit morceaux, et ajoute ceux
+				 * qui ne ne sont pas totalement couvert à la liste, sauf si son
+				 * aire est plus petite que le seuil d'acceptance. */
+
+				/* On coupe le triangle en quatre en introduisant un point au
+				 * centre de chaque coté. */
+				auto const v01 = (v0 + v1) * 0.5f;
+				auto const v12 = (v1 + v2) * 0.5f;
+				auto const v20 = (v2 + v0) * 0.5f;
+
+				Triangle triangle_fils[4] = {
+					Triangle{ v0, v01, v20},
+					Triangle{v01,  v1, v12},
+					Triangle{v12,  v2, v20},
+					Triangle{v20, v01, v12},
+				};
+
+				for (auto i = 0; i < 4; ++i) {
+
+					auto const aire = calcule_aire(triangle_fils[i]);
+
+					if (std::abs(aire - seuil_aire) <= std::numeric_limits<float>::epsilon()) {
+						boite->aire_totale -= aire;
+						continue;
+					}
+
+					couvert = triangle_couvert(triangle_fils[i], hachage_spatial, distance);
+
+					if (couvert) {
+						continue;
+					}
+
+					auto const index_boite0 = static_cast<int>(std::log2(aire_maximum / aire));
+
+					if (index_boite0 >= 0 && index_boite0 < 64) {
+						auto &b = boites[index_boite0];
+
+						b.triangles.ajoute(triangle_fils[i].v0, triangle_fils[i].v1, triangle_fils[i].v2);
+						b.aire_totale += aire;
+					}
+				}
+
+				boite->triangles.enleve(triangle);
+			}
+		}
+
+		std::cerr << "Nombre de points : " << points_nuage->taille() << "\n";
+		std::cerr << "Nombre d'alvéoles : " << hachage_spatial.taille() << '\n';
+		return EXECUTION_REUSSIE;
+	}
+};
+
+/* ************************************************************************** */
+
 void enregistre_operatrices_particules(UsineOperatrice &usine)
 {
 	usine.enregistre_type(cree_desc<OperatriceCreationPoints>());
 	usine.enregistre_type(cree_desc<OperatriceDispersionPoints>());
+	usine.enregistre_type(cree_desc<OperatriceTirageFleche>());
 }
 
 #pragma clang diagnostic pop
