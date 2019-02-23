98581ffdfb3aa75147f34f57728053c209aba918
Kévin Dietrich <kevin.dietrich@mailoo.org>
2019-02-23 14:56:31 +0100

    Simulation corps rigide : ajout d'un noeud spécifique pour créer les corps rigides dans le monde.

--- mikisa/coeur/operatrices/operatrices_bullet.cc
+++ mikisa/coeur/operatrices/operatrices_bullet.cc
@@ -45,6 +45,7 @@
 #include "bibliotheques/outils/definitions.hh"
 
 #include "../contexte_evaluation.hh"
+#include "../donnees_simulation.hh"
 #include "../operatrice_corps.h"
 #include "../usine_operatrice.h"
 
@@ -109,8 +110,9 @@ static btTransform converti_transformation(Corps &corps)
 	return trans;
 }
 
-/* À FAIRE : stockage des objets créés, ajournement, libération mémoire */
-class OperatriceDynCorpsRigide final : public OperatriceCorps {
+/* ************************************************************************** */
+
+class MondePhysique {
 	btDiscreteDynamicsWorld *m_monde_dynamics = nullptr;
 	btDefaultCollisionConfiguration *m_configuration_collision = nullptr;
 	btDispatcher *m_repartitrice = nullptr;
@@ -122,123 +124,25 @@ class OperatriceDynCorpsRigide final : public OperatriceCorps {
 	btAlignedObjectArray<btCollisionShape *> m_formes_collisions{};
 
 public:
-	static constexpr auto NOM = "Dynamiques Corps Rigides";
-	static constexpr auto AIDE = "";
-
-	explicit OperatriceDynCorpsRigide(Graphe &graphe_parent, Noeud *noeud)
-		: OperatriceCorps(graphe_parent, noeud)
-	{
-		entrees(1);
-		sorties(1);
-	}
-
-	OperatriceDynCorpsRigide(OperatriceDynCorpsRigide const &) = default;
-	OperatriceDynCorpsRigide &operator=(OperatriceDynCorpsRigide const &) = default;
-
-	~OperatriceDynCorpsRigide() override
+	~MondePhysique()
 	{
 		supprime_monde();
 	}
 
-	const char *nom_classe() const override
+	void ajoute_forme(btCollisionShape *forme)
 	{
-		return NOM;
+		m_formes_collisions.push_back(forme);
 	}
 
-	const char *texte_aide() const override
+	void ajoute_corps_rigide(btRigidBody *corps_rigide)
 	{
-		return AIDE;
-	}
-
-	int execute(ContexteEvaluation const &contexte) override
-	{
-		m_corps.reinitialise();
-		entree(0)->requiers_copie_corps(&m_corps, contexte);
-
-		/* À FAIRE : réinitialisation. */
-		if (m_monde_dynamics == nullptr || contexte.temps_courant == 1) {
-			initialise_monde();
-
-			auto forme = cree_forme_pour_corps(m_corps);
-			m_formes_collisions.push_back(forme);
-
-			auto tranformation = converti_transformation(m_corps);
-			auto masse = 1.0;
-
-			cree_corps_rigide(masse, tranformation, forme);
-		}
-
-		if (contexte.temps_courant > 1) {
-			m_monde_dynamics->stepSimulation(1.0 / 24.0);
-		}
-
-		ajourne_corps_depuis_sim();
-
-		return EXECUTION_REUSSIE;
-	}
-
-	void ajourne_corps_depuis_sim()
-	{
-		for (int i = m_monde_dynamics->getNumCollisionObjects() - 1; i >= 0; i--) {
-			btCollisionObject* obj = m_monde_dynamics->getCollisionObjectArray()[i];
-			btRigidBody* body = btRigidBody::upcast(obj);
-			auto ms = body->getMotionState();
-
-			if (ms != nullptr) {
-				btTransform trans;
-				ms->getWorldTransform(trans);
-
-				double mat[4][4];
-				trans.getOpenGLMatrix(reinterpret_cast<double *>(mat));
-				auto transformation = math::transformation(mat);
-				m_corps.transformation = transformation;
-			}
-		}
-	}
-
-	btRigidBody *cree_corps_rigide(double mass, btTransform const &transforme_initiale, btCollisionShape *forme_collision)
-	{
-		btAssert((!forme_collision || forme_collision->getShapeType() != INVALID_SHAPE_PROXYTYPE));
-
-		auto intertie_locale = btVector3(0, 0, 0);
-
-		/* Une masse différente de 0 définie un corps dynamique, c-à-d
-		 * non-static. */
-		if (mass != 0.0) {
-			forme_collision->calculateLocalInertia(mass, intertie_locale);
-		}
-
-		/* L'utilisation de btDefaultMotionState est recommandée, car cela
-		 * permet des interpolations, et synchronsie uniquement les objets
-		 * 'actifs'. */
-		auto etat_mouvement = new btDefaultMotionState(transforme_initiale);
-
-		auto infos = btRigidBody::btRigidBodyConstructionInfo(
-					mass,
-					etat_mouvement,
-					forme_collision,
-					intertie_locale);
-
-		auto corps_rigide = new btRigidBody(infos);
-		corps_rigide->setUserIndex(-1);
-		//corps_rigide->setContactProcessingThreshold(m_defaultContactProcessingThreshold);
-		corps_rigide->setFriction(0.5); /* défaut bullet */
-		corps_rigide->setRestitution(0.0);
-		corps_rigide->setDamping(0.04, 0.1);
-		corps_rigide->setSleepingThresholds(0.4, 0.5); /* moitié défaut bullet */
-		corps_rigide->setActivationState(1);
-		corps_rigide->setLinearFactor(btVector3(1, 1, 1)); /* 0 = vérouille position */
-		corps_rigide->setAngularFactor(btVector3(1, 1, 1)); /* 0 = vérouille axes */
-
-		/* cela désactive les mouvements pour quelque raison */
-	//	corps_rigide->setCollisionFlags(corps_rigide->getCollisionFlags() | btCollisionObject::CF_KINEMATIC_OBJECT);
-
 		m_monde_dynamics->addRigidBody(corps_rigide);
-
-		return corps_rigide;
 	}
 
-	/* ********************************************************************** */
+	btDiscreteDynamicsWorld *ptr()
+	{
+		return m_monde_dynamics;
+	}
 
 	void initialise_monde()
 	{
@@ -302,7 +206,189 @@ public:
 
 /* ************************************************************************** */
 
+class AjoutCorpsRigide final : public OperatriceCorps {
+public:
+	static constexpr auto NOM = "Ajout Corps Rigide";
+	static constexpr auto AIDE = "";
+
+	AjoutCorpsRigide(Graphe &graphe_parent, Noeud *noeud)
+		: OperatriceCorps(graphe_parent, noeud)
+	{
+		entrees(2);
+		sorties(1);
+	}
+
+	const char *chemin_entreface() const override
+	{
+		return "entreface/operatrice_ajout_corps_rigide.jo";
+	}
+
+	const char *nom_classe() const override
+	{
+		return NOM;
+	}
+
+	const char *texte_aide() const override
+	{
+		return AIDE;
+	}
+
+	int execute(ContexteEvaluation const &contexte) override
+	{
+		/* L'entrée 0 est pour le corps. */
+		m_corps.reinitialise();
+		entree(0)->requiers_copie_corps(&m_corps, contexte);
+
+		/* L'entrée 1 est pour accumuler les corps. */
+		entree(1)->requiers_corps(contexte);
+
+		auto monde = std::any_cast<MondePhysique *>(m_donnees_simulation->table["monde_physique"]);
+
+		auto forme = cree_forme_pour_corps(m_corps);
+		monde->ajoute_forme(forme);
+
+		auto tranformation = converti_transformation(m_corps);
+
+		auto corps_rigide = cree_corps_rigide(tranformation, forme);
+
+		monde->ajoute_corps_rigide(corps_rigide);
+
+		return EXECUTION_REUSSIE;
+	}
+
+	btRigidBody *cree_corps_rigide(btTransform const &transforme_initiale, btCollisionShape *forme_collision)
+	{
+		btAssert((!forme_collision || forme_collision->getShapeType() != INVALID_SHAPE_PROXYTYPE));
+
+		auto intertie_locale = btVector3(0, 0, 0);
+
+		/* Une masse différente de 0 définie un corps dynamique, c-à-d
+		 * non-static. */
+		auto masse = static_cast<double>(evalue_decimal("masse"));
+
+		if (masse != 0.0) {
+			forme_collision->calculateLocalInertia(masse, intertie_locale);
+		}
+
+		/* L'utilisation de btDefaultMotionState est recommandée, car cela
+		 * permet des interpolations, et synchronsie uniquement les objets
+		 * 'actifs'. */
+		auto etat_mouvement = new btDefaultMotionState(transforme_initiale);
+
+		auto infos = btRigidBody::btRigidBodyConstructionInfo(
+					masse,
+					etat_mouvement,
+					forme_collision,
+					intertie_locale);
+
+		auto const friction = evalue_decimal("friction");
+		auto const restitution = evalue_decimal("restitution");
+		auto const amort_lin = evalue_decimal("amort_lin");
+		auto const amort_ang = evalue_decimal("amort_ang");
+		auto const seuil_lin = evalue_decimal("seuil_lin");
+		auto const seuil_ang = evalue_decimal("seuil_ang");
+
+		auto corps_rigide = new btRigidBody(infos);
+		corps_rigide->setUserIndex(-1);
+		//corps_rigide->setContactProcessingThreshold(m_defaultContactProcessingThreshold);
+		corps_rigide->setFriction(static_cast<double>(friction));
+		corps_rigide->setRestitution(static_cast<double>(restitution));
+		corps_rigide->setDamping(
+					static_cast<double>(amort_lin),
+					static_cast<double>(amort_ang));
+		corps_rigide->setSleepingThresholds(
+					static_cast<double>(seuil_lin),
+					static_cast<double>(seuil_ang));
+		corps_rigide->setActivationState(1);
+		corps_rigide->setLinearFactor(btVector3(1, 1, 1)); /* 0 = vérouille position */
+		corps_rigide->setAngularFactor(btVector3(1, 1, 1)); /* 0 = vérouille axes */
+
+		/* cela désactive les mouvements pour quelque raison */
+	//	corps_rigide->setCollisionFlags(corps_rigide->getCollisionFlags() | btCollisionObject::CF_KINEMATIC_OBJECT);
+
+		return corps_rigide;
+	}
+};
+
+/* ************************************************************************** */
+
+/* À FAIRE : stockage des objets créés, ajournement, libération mémoire */
+class OperatriceDynCorpsRigide final : public OperatriceCorps {
+	MondePhysique m_monde{};
+
+public:
+	static constexpr auto NOM = "Dynamiques Corps Rigides";
+	static constexpr auto AIDE = "";
+
+	explicit OperatriceDynCorpsRigide(Graphe &graphe_parent, Noeud *noeud)
+		: OperatriceCorps(graphe_parent, noeud)
+	{
+		entrees(1);
+		sorties(1);
+	}
+
+	OperatriceDynCorpsRigide(OperatriceDynCorpsRigide const &) = default;
+	OperatriceDynCorpsRigide &operator=(OperatriceDynCorpsRigide const &) = default;
+
+	const char *nom_classe() const override
+	{
+		return NOM;
+	}
+
+	const char *texte_aide() const override
+	{
+		return AIDE;
+	}
+
+	int execute(ContexteEvaluation const &contexte) override
+	{
+		m_donnees_simulation->table.insert({ "monde_physique", &m_monde });
+
+		/* À FAIRE : réinitialisation. */
+		if (m_monde.ptr() == nullptr || contexte.temps_courant == m_donnees_simulation->temps_debut) {
+			m_monde.initialise_monde();
+
+			/* il faut exécuter les noeuds en amont après avoir initialisé la table */
+			m_corps.reinitialise();
+			entree(0)->requiers_copie_corps(&m_corps, contexte);
+		}
+
+		if (contexte.temps_courant > m_donnees_simulation->temps_debut) {
+			m_monde.ptr()->stepSimulation(1.0 / contexte.cadence);
+		}
+
+		ajourne_corps_depuis_sim();
+
+		return EXECUTION_REUSSIE;
+	}
+
+	void ajourne_corps_depuis_sim()
+	{
+		/* À FAIRE :
+		 * - pour modifier les matrices pour chaque objet, il vaudrait mieux
+		 *   pouvoir accéder aux corps des objets via des pointeurs. */
+		for (int i = 0; i < m_monde.ptr()->getNumCollisionObjects(); i++) {
+			btCollisionObject* obj = m_monde.ptr()->getCollisionObjectArray()[i];
+			btRigidBody* body = btRigidBody::upcast(obj);
+			auto ms = body->getMotionState();
+
+			if (ms != nullptr) {
+				btTransform trans;
+				ms->getWorldTransform(trans);
+
+				double mat[4][4];
+				trans.getOpenGLMatrix(reinterpret_cast<double *>(mat));
+				auto transformation = math::transformation(mat);
+				m_corps.transformation = transformation;
+			}
+		}
+	}
+};
+
+/* ************************************************************************** */
+
 void enregistre_operatrices_bullet(UsineOperatrice &usine)
 {
+	usine.enregistre_type(cree_desc<AjoutCorpsRigide>());
 	usine.enregistre_type(cree_desc<OperatriceDynCorpsRigide>());
 }
