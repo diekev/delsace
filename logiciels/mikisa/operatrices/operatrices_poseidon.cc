671150afc85e8533d07ccc15de759a373c2300ed
Kévin Dietrich <kevin.dietrich@mailoo.org>
2019-08-04 22:06:28 +0200

    Mikisa, Poséidon : ajout d'une opératrice pour créer de la vorticité.
    Cela mène à un fluide très instable....

--- logiciels/mikisa/operatrices/operatrices_poseidon.cc
+++ logiciels/mikisa/operatrices/operatrices_poseidon.cc
@@ -518,6 +518,156 @@ auto calcul_dt(Poseidon &poseidon, float vel_max)
 #endif
 }
 
+/* À FAIRE : instable. */
+void ajoute_vorticite(
+		Poseidon &poseidon,
+		float quantite_vorticite,
+		float vorticite_flamme,
+		float dt)
+{
+	auto &drapeaux = *poseidon.drapeaux;
+	auto &velocite = poseidon.velocite;
+	/* À FAIRE */
+	auto fioul = static_cast<grille_dense_3d<float> *>(nullptr);
+	auto desc = velocite->desc();
+	auto res = desc.resolution;
+	auto dalle_x = 1;
+	auto dalle_y = res.x;
+	auto dalle_z = res.x + res.y;
+	auto mi_dx_inv = static_cast<float>(0.5 / desc.taille_voxel);
+	auto taill_voxel = static_cast<float>(desc.taille_voxel);
+	auto dx_inv = static_cast<float>(1.0 / desc.taille_voxel);
+	auto max_res = max(res);
+
+	auto echelle_constante = 64.0 / static_cast<double>(max_res);
+	echelle_constante = (echelle_constante < 1.0) ? 1.0 : echelle_constante;
+
+	vorticite_flamme /= static_cast<float>(echelle_constante);
+
+	if (quantite_vorticite + vorticite_flamme <= 0.0f) {
+		return;
+	}
+
+	desc.type_donnees = type_grille::R32;
+	auto vorticite_x = grille_dense_3d<float>(desc);
+	auto vorticite_y = grille_dense_3d<float>(desc);
+	auto vorticite_z = grille_dense_3d<float>(desc);
+	auto vorticite   = grille_dense_3d<float>(desc);
+
+//	objvelocity[0] = _xVelocityOb;
+//	objvelocity[1] = _yVelocityOb;
+//	objvelocity[2] = _zVelocityOb;
+
+	auto vort_vel = [&](int obpos[6], int i, int j)
+	{
+		/* obstacle animé */
+//		if (_obstacles[obpos[(i)]] & 8) {
+//			((abs(objvelocity[(j)][obpos[(i)]]) > FLT_EPSILON) ? objvelocity[(j)][obpos[(i)]] : velocity[(j)][index]);
+//		}
+
+		return velocite->valeur(obpos[i])[static_cast<size_t>(j)];
+	};
+
+	auto index = dalle_x + dalle_y + dalle_z;
+
+	for (int z = 1; z < res.z - 1; z++, index += res.x * res.x) {
+		for (int y = 1; y < res.y - 1; y++, index += 2) {
+			for (int x = 1; x < res.x - 1; x++, index++) {
+				if (est_obstacle(drapeaux, index)) {
+					continue;
+				}
+
+				int obpos[6];
+
+				obpos[0] = (est_obstacle(drapeaux, index + dalle_y)) ? index : index + dalle_y; // up
+				obpos[1] = (est_obstacle(drapeaux, index - dalle_y)) ? index : index - dalle_y; // down
+				float dy = (obpos[0] == index || obpos[1] == index) ? dx_inv : mi_dx_inv;
+
+				obpos[2]  = (est_obstacle(drapeaux, index + dalle_z)) ? index : index + dalle_z; // out
+				obpos[3]  = (est_obstacle(drapeaux, index - dalle_z)) ? index : index - dalle_z; // in
+				float dz  = (obpos[2] == index || obpos[3] == index) ? dx_inv : mi_dx_inv;
+
+				obpos[4] = (est_obstacle(drapeaux, index + dalle_x)) ? index : index + dalle_x; // right
+				obpos[5] = (est_obstacle(drapeaux, index - dalle_x)) ? index : index - dalle_x; // left
+				float dx = (obpos[4] == index || obpos[5] == index) ? dx_inv : mi_dx_inv;
+
+				float xV[2], yV[2], zV[2];
+
+				zV[1] = vort_vel(obpos, 0, 2);
+				zV[0] = vort_vel(obpos, 1, 2);
+				yV[1] = vort_vel(obpos, 2, 1);
+				yV[0] = vort_vel(obpos, 3, 1);
+				vorticite_x.valeur(index) = (zV[1] - zV[0]) * dy + (-yV[1] + yV[0]) * dz;
+
+				xV[1] = vort_vel(obpos, 2, 0);
+				xV[0] = vort_vel(obpos, 3, 0);
+				zV[1] = vort_vel(obpos, 4, 2);
+				zV[0] = vort_vel(obpos, 5, 2);
+				vorticite_y.valeur(index) = (xV[1] - xV[0]) * dz + (-zV[1] + zV[0]) * dx;
+
+				yV[1] = vort_vel(obpos, 4, 1);
+				yV[0] = vort_vel(obpos, 5, 1);
+				xV[1] = vort_vel(obpos, 0, 0);
+				xV[0] = vort_vel(obpos, 1, 0);
+				vorticite_z.valeur(index) = (yV[1] - yV[0]) * dx + (-xV[1] + xV[0])* dy;
+
+				vorticite.valeur(index) = sqrtf(vorticite_x.valeur(index) * vorticite_x.valeur(index) +
+												 vorticite_y.valeur(index) * vorticite_y.valeur(index) +
+												 vorticite_z.valeur(index) * vorticite_z.valeur(index));
+			}
+		}
+	}
+
+	// calculate normalized vorticity vectors
+	float eps = quantite_vorticite;
+
+	index = dalle_x + dalle_y + dalle_z;
+
+	for (int z = 1; z < res.z - 1; z++, index += res.x * res.x) {
+		for (int y = 1; y < res.y - 1; y++, index += 2) {
+			for (int x = 1; x < res.x - 1; x++, index++) {
+				if (est_obstacle(drapeaux, index)) {
+					continue;
+				}
+
+				float N[3];
+
+				auto up    = (est_obstacle(drapeaux, index + dalle_y)) ? index : index + dalle_y;
+				auto down  = (est_obstacle(drapeaux, index - dalle_y)) ? index : index - dalle_y;
+				auto dy  = (up == index || down == index) ? dx_inv : mi_dx_inv;
+
+				auto out   = (est_obstacle(drapeaux, index + dalle_z)) ? index : index + dalle_z;
+				auto in    = (est_obstacle(drapeaux, index - dalle_z)) ? index : index - dalle_z;
+				auto dz  = (out == index || in == index) ? dx_inv : mi_dx_inv;
+
+				auto right = (est_obstacle(drapeaux, index + dalle_x)) ? index : index + dalle_x;
+				auto left  = (est_obstacle(drapeaux, index - dalle_x)) ? index : index - dalle_x;
+				auto dx  = (right == index || left == index) ? dx_inv : mi_dx_inv;
+
+				N[0] = (vorticite.valeur(right) - vorticite.valeur(left)) * dx;
+				N[1] = (vorticite.valeur(up) - vorticite.valeur(down)) * dy;
+				N[2] = (vorticite.valeur(out) - vorticite.valeur(in)) * dz;
+
+				auto magnitude = std::sqrt(N[0] * N[0] + N[1] * N[1] + N[2] * N[2]);
+
+				if (magnitude > std::numeric_limits<float>::epsilon()) {
+					auto flame_vort = (fioul != nullptr) ? fioul->valeur(index) * vorticite_flamme : 0.0f;
+					magnitude = 1.0f / magnitude;
+					N[0] *= magnitude;
+					N[1] *= magnitude;
+					N[2] *= magnitude;
+
+					auto &vel = velocite->valeur(index);
+
+					vel.x += (N[1] * vorticite_z.valeur(index) - N[2] * vorticite_y.valeur(index)) * taill_voxel * (eps + flame_vort) * dt;
+					vel.y += (N[2] * vorticite_x.valeur(index) - N[0] * vorticite_z.valeur(index)) * taill_voxel * (eps + flame_vort) * dt;
+					vel.z += (N[0] * vorticite_y.valeur(index) - N[1] * vorticite_x.valeur(index)) * taill_voxel * (eps + flame_vort) * dt;
+				}
+			}
+		}
+	}
+}
+
 }
 
 class OpSimulationGaz : public OperatriceCorps {
@@ -956,6 +1106,63 @@ public:
 
 /* ************************************************************************** */
 
+class OpVorticiteGaz : public OperatriceCorps {
+public:
+	static constexpr auto NOM = "Vorticité Gaz";
+	static constexpr auto AIDE = "";
+
+	explicit OpVorticiteGaz(Graphe &graphe_parent, Noeud *noeud)
+		: OperatriceCorps(graphe_parent, noeud)
+	{
+		m_execute_toujours = true;
+		entrees(1);
+	}
+
+	const char *chemin_entreface() const override
+	{
+		return "";
+	}
+
+	const char *nom_classe() const override
+	{
+		return NOM;
+	}
+
+	const char *texte_aide() const override
+	{
+		return AIDE;
+	}
+
+	int execute(ContexteEvaluation const &contexte, DonneesAval *donnees_aval) override
+	{
+		m_corps.reinitialise();
+
+		if (!donnees_aval || !donnees_aval->possede("poseidon")) {
+			this->ajoute_avertissement("Il n'y a pas de simulation de gaz en aval.");
+			return EXECUTION_ECHOUEE;
+		}
+
+		/* accumule les entrées */
+		entree(0)->requiers_corps(contexte, donnees_aval);
+
+		/* passe à notre exécution */
+		auto poseidon_gaz = extrait_poseidon(donnees_aval);
+
+		auto quantite_vorticite = 2.0f;
+		auto vorticite_flamme = 0.5f;
+
+		psn::ajoute_vorticite(
+					*poseidon_gaz,
+					quantite_vorticite,
+					vorticite_flamme,
+					poseidon_gaz->dt);
+
+		return EXECUTION_REUSSIE;
+	}
+};
+
+/* ************************************************************************** */
+
 void enregistre_operatrices_poseidon(UsineOperatrice &usine)
 {
 	usine.enregistre_type(cree_desc<OpEntreeGaz>());
@@ -964,4 +1171,5 @@ void enregistre_operatrices_poseidon(UsineOperatrice &usine)
 	usine.enregistre_type(cree_desc<OpAdvectionGaz>());
 	usine.enregistre_type(cree_desc<OpFlottanceGaz>());
 	usine.enregistre_type(cree_desc<OpIncompressibiliteGaz>());
+	usine.enregistre_type(cree_desc<OpVorticiteGaz>());
 }
