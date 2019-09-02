afc3ebcffd62d4813c06d656197a00768af96458
Kévin Dietrich <kevin.dietrich@mailoo.org>
2019-09-03 00:52:31 +0200

    Koudou : ajout d'un code test pour un échantillonnage basé sur l'algorithme CMJ de Pixar.

--- logiciels/mikisa/koudou/moteur_rendu.cc
+++ logiciels/mikisa/koudou/moteur_rendu.cc
@@ -44,9 +44,78 @@ namespace kdo {
 
 /* ************************************************************************** */
 
+#if 0
+struct xy {
+	float x;
+	float y;
+};
+
+unsigned permute(unsigned i, unsigned l, unsigned p)
+{
+	unsigned w = l - 1;
+	w |= w >> 1;
+	w |= w >> 2;
+	w |= w >> 4;
+	w |= w >> 8;
+	w |= w >> 16;
+	i ^= p; i *= 0xe170893d;
+	i ^= p >> 16;
+	do {
+		i ^= p;
+		i *= 0xe17093d;
+		i ^= p       >> 16;
+		i ^= (i & w) >> 4;
+		i ^= p       >> 8; i *= 0x0929eb3f;
+		i ^= p       >> 23;
+		i ^= (i & w) >> 1; i *= 1 | p >> 27;
+		i *= 0x6935fa69;
+		i ^= (i & w) >> 11; i *= 0x74dcb303;
+		i ^= (i & w) >>  2; i *= 0x9e501cc3;
+		i ^= (i & w) >>  2; i *= 0xc860a3df;
+		i &= w;
+		i ^= i >> 5;
+	} while (i >= l);
+
+	return (i + p) % l;
+}
+
+float randfloat(unsigned i, unsigned p)
+{
+	i ^= p;
+	i ^= i >> 17;
+	i ^= i >> 10; i *= 0xb36534e5;
+	i ^= i >> 12;
+	i ^= i >> 21; i *= 0x93fc4795;
+	i ^= 0xdf6e307f;
+	i ^= i >> 17; i *= 1 | p >> 18;
+	return i * (1.0f / 4294967808.0f);
+}
+
+auto cmj(int s, int m, int n, int p)
+{
+	int sx = permute(s % m, m, p * 0xa511e9b3);
+	int sy = permute(s / m, n, p * 0x63d83595);
+	float jx = randfloat(s, p * 0xa399d265);
+	float jy = randfloat(s, p * 0x711ad6a5);
+	xy r = {(s % m + (sy + jx) / n) / m, (s / m + (sx + jy) / m) / n};
+
+	return r;
+}
+
+auto genere_echantillon_cmj(int s, int m, int n, int p)
+{
+	auto xj = cmj(s, m, n, p);
+	auto echantillon = vision::EchantillonCamera();
+	echantillon.x = static_cast<double>(xj.x);
+	echantillon.y = static_cast<double>(xj.y);
+	return echantillon;
+}
+#endif
+
 vision::EchantillonCamera genere_echantillon(GNA &gna, unsigned int x, unsigned int y)
 {
 	vision::EchantillonCamera echantillon;
+
 	echantillon.x = x + gna.uniforme(0.0, 1.0);
 	echantillon.y = y + gna.uniforme(0.0, 1.0);
 
@@ -212,6 +281,7 @@ void MoteurRendu::echantillone_scene(ParametresRendu const &parametres, dls::tab
 
 			for (auto x = carreau.x; x < carreau.x + carreau.largeur; ++x) {
 				for (auto y = carreau.y; y < carreau.y + carreau.hauteur; ++y) {
+					//auto echantillon_camera = genere_echantillon_cmj(x + camera->largeur() * y, camera->hauteur(), camera->largeur(), echantillon);
 					auto echantillon_camera = genere_echantillon(gna, x, y);
 					echants.pousse(echantillon_camera);
 
