3c314ffa20897d4e8e71f54a5594bd91bab9cc59
Kévin Dietrich <kevin.dietrich@mailoo.org>
2019-09-23 19:30:27 +0200

    Mikisa : ajout d'une opératrice pour créer des points dans la vue 3d depuis une image.
    Utile pour visionner des histogrammes pour l'étalonnage.

--- logiciels/mikisa/coeur/mikisa.cc
+++ logiciels/mikisa/coeur/mikisa.cc
@@ -79,6 +79,7 @@
 #include "operatrices/operatrices_fracture.hh"
 #include "operatrices/operatrices_groupes.hh"
 #include "operatrices/operatrices_image_profonde.hh"
+#include "operatrices/operatrices_images_3d.hh"
 #include "operatrices/operatrices_maillage.hh"
 #include "operatrices/operatrices_muscles.hh"
 #include "operatrices/operatrices_ocean.hh"
@@ -156,6 +157,7 @@ void Mikisa::initialise()
 	enregistre_operatrices_flux(m_usine_operatrices);
 	enregistre_operatrices_fracture(m_usine_operatrices);
 	enregistre_operatrices_image_profonde(m_usine_operatrices);
+	enregistre_operatrices_images_3d(m_usine_operatrices);
 	enregistre_operatrices_groupes(m_usine_operatrices);
 	enregistre_operatrices_maillage(m_usine_operatrices);
 	enregistre_operatrices_muscles(m_usine_operatrices);
