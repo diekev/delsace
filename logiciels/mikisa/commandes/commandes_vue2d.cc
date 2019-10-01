2edf9e09dac62cdfe219386fbc6f059ac51c65b0
Kévin Dietrich <kevin.dietrich@mailoo.org>
2019-10-01 21:11:07 +0200

    Mikisa : début de travail sur une commande pour peindre une image.

--- logiciels/mikisa/commandes/commandes_vue2d.cc
+++ logiciels/mikisa/commandes/commandes_vue2d.cc
@@ -40,6 +40,7 @@
 #include "coeur/composite.h"
 #include "coeur/evenement.h"
 #include "coeur/mikisa.h"
+#include "coeur/operatrice_image.h"
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wweak-vtables"
@@ -97,6 +98,69 @@ public:
 
 /* ************************************************************************** */
 
+struct CommandeOutil2D final : public Commande {
+	float vieil_x = 0.0f;
+	float vieil_y = 0.0f;
+
+	int execute(std::any const &pointeur, DonneesCommande const &donnees) override
+	{
+		auto mikisa = extrait_mikisa(pointeur);
+
+		/* vérifie si le clique est dans l'image */
+
+		auto const &noeud_composite = mikisa->bdd.graphe_composites()->noeud_actif;
+
+		if (noeud_composite == nullptr) {
+			return EXECUTION_COMMANDE_ECHOUEE;
+		}
+
+		auto composite = extrait_composite(noeud_composite->donnees);
+		auto const &image = composite->image();
+
+		auto calque = image.calque_pour_lecture("image");
+
+		if (calque == nullptr) {
+			return EXECUTION_COMMANDE_ECHOUEE;
+		}
+
+		auto camera_2d = mikisa->camera_2d;
+		/* converti en espace -1:1 */
+		auto x = (static_cast<float>(camera_2d->largeur) - donnees.x);
+		auto y = (static_cast<float>(camera_2d->hauteur) - donnees.y);
+
+		x -= static_cast<float>(camera_2d->largeur / 2);
+		y -= static_cast<float>(camera_2d->hauteur / 2);
+
+		/* converti en espace monde */
+		x = (x / camera_2d->zoom) - camera_2d->pos_x;
+		y = (y / camera_2d->zoom) - camera_2d->pos_y;
+
+		std::cerr << "Pos écran : " << donnees.x << ", " << donnees.y << '\n';
+		std::cerr << "Pos monde : " << x << ", " << y << '\n';
+
+		auto co = dls::math::vec2f(x, y);
+
+		auto tampon = extrait_grille_couleur(calque);
+
+		if (tampon->hors_des_limites(tampon->monde_vers_index(co))) {
+			std::cerr << "Le clique est HORS des limites du calque...\n";
+		}
+		else {
+			std::cerr << "Le clique est DANS les limites du calque...\n";
+		}
+
+		return EXECUTION_COMMANDE_MODALE;
+	}
+
+	void ajourne_execution_modale(std::any const &pointeur, DonneesCommande const &donnees) override
+	{
+		INUTILISE(pointeur);
+		INUTILISE(donnees);
+	}
+};
+
+/* ************************************************************************** */
+
 void enregistre_commandes_vue2d(UsineCommande &usine)
 {
 	usine.enregistre_type("commande_zoom_camera_2d",
@@ -106,6 +170,10 @@ void enregistre_commandes_vue2d(UsineCommande &usine)
 	usine.enregistre_type("commande_pan_camera_2d",
 						   description_commande<CommandePanCamera2D>(
 							   "vue_2d", Qt::MiddleButton, Qt::ShiftModifier, 0, false));
+
+	usine.enregistre_type("commande_outil_2d",
+						   description_commande<CommandeOutil2D>(
+							   "vue_2d", Qt::LeftButton, 0, 0, false));
 }
 
 #pragma clang diagnostic pop
