2835da8cebf60541e4e5ed1616b13f852a05aab6
Kévin Dietrich <kevin.dietrich@mailoo.org>
2019-12-08 18:49:44 +0100

    nettoyage : suppression de la bibliothèque réseau
    Elle n'était pas complète et est en cours de réécriture via Kuri.

--- tests/tests.cc
+++ tests/tests.cc
@@ -24,8 +24,6 @@
 
 #include "tests.hh"
 
-//#include "test_uri.hh"
-
 #include "math/tests_math.hh"
 #include "math/tests_matrice.hh"
 #include "math/tests_quaternion.hh"
@@ -34,7 +32,6 @@
 int main()
 {
 	dls::test_unitaire::Controleuse controleuse;
-	//controleuse.ajoute_fonction(test_uri);
 	controleuse.ajoute_fonction(tests_matrice);
 	controleuse.ajoute_fonction(tests_quaternion);
 	controleuse.ajoute_fonction(tests_vecteur);
