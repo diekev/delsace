ce38cc2469c2b6533df78495e8e46bc3b3c484a4
Kévin Dietrich <kevin.dietrich@mailoo.org>
2020-02-01 19:00:16 +0100

    nettoyage(kuri) : suppression de la bibliothèque base64
    La bibliothèque n'était là que pour servir dans une implémentation en C+
    + des sites Python, et a été réimplémentée en Kuri.

--- tests/tests_aleatoire.cc
+++ tests/tests_aleatoire.cc
@@ -26,25 +26,7 @@
 
 #include "biblinternes/tests/test_aleatoire.hh"
 
-#include "biblinternes/base64/base64.hh"
 #include "biblinternes/sha256/sha256.hh"
-
-int test_aleatoire_base64_encode(const u_char *donnees, size_t taille)
-{
-	base64::encode(donnees, taille);
-	base64::encode_pour_url(donnees, taille);
-
-	return 0;
-}
-
-int test_aleatoire_base64_decode(const u_char *donnees, long taille)
-{
-	dls::chaine chaine(reinterpret_cast<const char *>(donnees), taille);
-	base64::decode(chaine);
-	base64::decode_pour_url(chaine);
-
-	return 0;
-}
 
 int test_aleatoire_sha256(const u_char *donnees, long taille)
 {
@@ -57,8 +39,6 @@ int test_aleatoire_sha256(const u_char *donnees, long taille)
 int main()
 {
 	dls::test_aleatoire::Testeuse testeur;
-	testeur.ajoute_tests("base64_encode", nullptr, test_aleatoire_base64_encode);
-	testeur.ajoute_tests("base64_decode", nullptr, test_aleatoire_base64_decode);
 	testeur.ajoute_tests("sha256", nullptr, test_aleatoire_sha256);
 
 	return testeur.performe_tests(std::cerr);
