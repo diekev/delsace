1b8ed737c155a0da1e57d2323aa57525f721b9db
Kévin Dietrich <kevin.dietrich@mailoo.org>
2020-02-27 15:35:36 +0100

    dév(kuri) : début de travail sur un module pour freetype gl, et son utilisation dans un projet

---
+++ logiciels/kuri/modules_c/FTGL/freetype-gl.h
@@ -0,0 +1,16 @@
+/* Freetype GL - A C OpenGL Freetype engine
+ *
+ * Distributed under the OSI-approved BSD 2-Clause License.  See accompanying
+ * file `LICENSE` for more details.
+ */
+#ifndef __FREETYPE_GL_H__
+#define __FREETYPE_GL_H__
+
+/* Mandatory */
+#include "opengl.h"
+#include "vec234.h"
+#include "vector.h"
+#include "texture-atlas.h"
+#include "texture-font.h"
+
+#endif /* FREETYPE_GL_H */
