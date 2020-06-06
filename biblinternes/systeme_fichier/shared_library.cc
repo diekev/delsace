aacaa41d0344a95c231ca43de43ca3fd82fb00c6
Kévin Dietrich <kevin.dietrich@mailoo.org>
2020-06-06 10:30:27 +0200

    correction(biblinternes) : crash lors d'un chargement erroné d'un objet dynamique
    Le problème étant que ec.assign prend le pointeur de son deuxième
    argument, ce qui dans notre cas résulte en un pointeur de la pile, ce
    qui l'invalide.

--- biblinternes/systeme_fichier/shared_library.cc
+++ biblinternes/systeme_fichier/shared_library.cc
@@ -56,6 +56,8 @@ public:
 	}
 };
 
+static dynamic_loading_category erreur_chargement;
+
 static void *get_symbol(void *handle, const dls::chaine &name, std::error_code &ec)
 {
 	dlerror();  /* clear any existing error */
@@ -151,8 +153,7 @@ void shared_library::open(const std::filesystem::path &filename, dso_loading fla
 	open(filename, ec, flag);
 
 	if (ec != std::error_code()) {
-		//const dls::chaine error = dlerror();
-		throw std::filesystem::filesystem_error("Cannot open shared library", m_chemin.c_str(), filename, ec);
+		throw std::filesystem::filesystem_error(dlerror(), m_chemin.c_str(), filename, ec);
 	}
 }
 
@@ -164,7 +165,7 @@ void shared_library::open(const std::filesystem::path &filename, std::error_code
 
 	if (m_handle != nullptr) {
 		if (dlclose(m_handle) != 0) {
-			ec.assign(1, __detail::dynamic_loading_category());
+			ec.assign(1, __detail::erreur_chargement);
 			return;
 		}
 	}
@@ -172,7 +173,7 @@ void shared_library::open(const std::filesystem::path &filename, std::error_code
 	m_handle = dlopen(filename.c_str(), static_cast<int>(flag));
 
 	if (m_handle == nullptr) {
-		ec.assign(1, __detail::dynamic_loading_category());
+		ec.assign(1, __detail::erreur_chargement);
 		return;
 	}
 
