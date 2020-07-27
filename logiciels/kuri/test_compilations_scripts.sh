5087e2511645ba7f2211ee3cd329380d8833c864
Kévin Dietrich <kevin.dietrich@mailoo.org>
2020-07-27 21:34:42 +0200

    dév(kuri) : ajout d'un script pour compiler tous les programmes que nous avons (il en manque, mais c'est un début), et corrige pas mal d'erreurs de compilation

---
+++ logiciels/kuri/test_compilations_scripts.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# cas pour les dossiers
+
+DOSSIER_COURANT=`pwd`
+
+DOSSIERS=("exemples/applications/BDD" \
+          "exemples/applications/EditeurTexte" \
+          "exemples/applications/Eliza" \
+          "exemples/applications/Jeu" \
+          "exemples/applications/MoteurRendu" \
+          "exemples/applications/Site")
+
+for dossier in ${DOSSIERS[@]}; do
+    cd "$DOSSIER_COURANT/$dossier"
+
+    echo "Compilation de :" $dossier
+    kuri principale.kuri > /dev/null
+done
+
+cd $DOSSIER_COURANT
+
+# cas pour les fichiers simples
+
+# À FAIRE : exemples/applications/jeu_chaos.kuri
+# À FAIRE : "exemples/applications/json.kuri" \ crash typage retours multiples
+# À FAIRE : "exemples/applications/serveuse.kuri" \ tente piège err n'a pas de type
+# À FAIRE : "exemples/applications/client_smtp.kuri" \ tente piège err n'a pas de type
+# À FAIRE : "exemples/applications/serveuse_ssl.kuri" \ tente piège err n'a pas de type
+# À FAIRE : "exemples/applications/cliente.kuri" \ tente piège err retourn mauvais type
+FICHIERS=("exemples/applications/base64.kuri" \
+          "exemples/applications/calendrier_républicain.kuri" \
+          "exemples/applications/crée_test_compilateur.kuri" \
+          "exemples/applications/labyrinthe.kuri")
+
+for fichier in ${FICHIERS[@]}; do
+    echo "Compilation de :" $fichier
+    kuri $fichier > /dev/null
+done
