a2a419727a595c37e03c1a07f2a442842e78c418
Kévin Dietrich <kevin.dietrich@mailoo.org>
2020-07-28 22:35:56 +0200

    dév(kuri) : ajoute plusieurs fichiers aux tests de compilation, et corrige certaines erreurs dans ceux-ci
    Supprime également deux fichiers inutiles.

--- logiciels/kuri/test_compilations_scripts.sh
+++ logiciels/kuri/test_compilations_scripts.sh
@@ -23,6 +23,8 @@ cd $DOSSIER_COURANT
 # cas pour les fichiers simples
 
 # À FAIRE : exemples/applications/jeu_chaos.kuri
+# À FAIRE : exemples/demos/demo_coroutine.kuri
+# À FAIRE : exemples/demos/demo_coro_nombre_premier.kuri
 FICHIERS=("exemples/applications/base64.kuri" \
           "exemples/applications/calendrier_républicain.kuri" \
           "exemples/applications/cliente.kuri" \
@@ -31,7 +33,26 @@ FICHIERS=("exemples/applications/base64.kuri" \
           "exemples/applications/json.kuri" \
           "exemples/applications/labyrinthe.kuri" \
           "exemples/applications/serveuse.kuri" \
-          "exemples/applications/serveuse_ssl.kuri")
+          "exemples/applications/serveuse_ssl.kuri" \
+          "exemples/demos/demo_expansion_argument_variadique.kuri" \
+          "exemples/demos/demo_gabarit.kuri" \
+          "exemples/demos/demo_info_type_fonction.kuri" \
+          "exemples/demos/demo_logement.kuri" \
+          "exemples/demos/demo_operateur_crochet.kuri" \
+          "exemples/demos/demo_operateur_point.kuri" \
+          "exemples/demos/demo_reference.kuri" \
+          "exemples/demos/demo_retour_multiple.kuri" \
+          "exemples/demos/demo_surcharge_fonctions.kuri" \
+          "exemples/demos/demo_syntax_appel_uniforme.kuri" \
+          "exemples/demos/demo_typage.kuri" \
+          "exemples/demos/demo_var_emploiee.kuri" \
+          "exemples/doc_kuri_dls/ex01_bonjour.kuri" \
+          "exemples/doc_kuri_dls/ex02_inference.kuri" \
+          "exemples/doc_kuri_dls/ex03_constance.kuri" \
+          "exemples/doc_kuri_dls/ex04_tableau.kuri" \
+          "exemples/doc_kuri_dls/ex05_fonc_variadique.kuri" \
+          "exemples/doc_kuri_dls/ex06_introspection.kuri" \
+          "exemples/doc_kuri_dls/ex07_coroutine.kuri")
 
 for fichier in ${FICHIERS[@]}; do
     echo "Compilation de :" $fichier
