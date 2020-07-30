b5160b4d651f6485b67ebee608434707806a71dc
Kévin Dietrich <kevin.dietrich@mailoo.org>
2020-07-31 00:58:00 +0200

    dév(kuri) : ajout d'une application exemple pour résoudre des sudokus

--- logiciels/kuri/test_compilations_scripts.sh
+++ logiciels/kuri/test_compilations_scripts.sh
@@ -36,6 +36,7 @@ FICHIERS=("exemples/applications/base64.kuri" \
           "exemples/applications/labyrinthe.kuri" \
           "exemples/applications/serveuse.kuri" \
           "exemples/applications/serveuse_ssl.kuri" \
+          "exemples/applications/sudoku.kuri" \
           "exemples/demos/demo_expansion_argument_variadique.kuri" \
           "exemples/demos/demo_gabarit.kuri" \
           "exemples/demos/demo_info_type_fonction.kuri" \
