9c35b29168cd2851cbaaebb39069f659699618f1
Kévin Dietrich <kevin.dietrich@mailoo.org>
2021-03-12 20:53:49 +0100

    dév(kuri) : ajout de BaseSyntaxeuse pour abstraire des opérations de base pour le syntaxage

---
+++ logiciels/kuri/compilation/base_syntaxeuse.cc
@@ -0,0 +1,91 @@
+/*
+ * ***** BEGIN GPL LICENSE BLOCK *****
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software  Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The Original Code is Copyright (C) 2021 Kévin Dietrich.
+ * All rights reserved.
+ *
+ * ***** END GPL LICENSE BLOCK *****
+ *
+ */
+
+#include "base_syntaxeuse.hh"
+
+#include "erreur.h"
+#include "modules.hh"
+
+BaseSyntaxeuse::BaseSyntaxeuse(Fichier *fichier)
+	: m_lexemes(fichier->donnees_constantes->lexemes)
+	, m_fichier(fichier)
+{
+	if (m_lexemes.taille() > 0) {
+		m_lexeme_courant = &m_lexemes[0];
+	}
+}
+
+void BaseSyntaxeuse::analyse()
+{
+	m_position = 0;
+
+	m_fichier->temps_analyse = 0.0;
+	quand_commence();
+
+	if (m_lexemes.taille() == 0) {
+		quand_termine();
+		return;
+	}
+
+	m_chrono_analyse.commence();
+
+	while (!fini() && !possede_erreur()) {
+		analyse_une_chose();
+	}
+
+	m_fichier->temps_analyse += m_chrono_analyse.arrete();
+	quand_termine();
+}
+
+void BaseSyntaxeuse::empile_etat(kuri::chaine_statique message, Lexeme *lexeme)
+{
+	m_donnees_etat_syntaxage.ajoute({ lexeme, message });
+}
+
+void BaseSyntaxeuse::depile_etat()
+{
+	m_donnees_etat_syntaxage.pop_back();
+}
+
+kuri::chaine BaseSyntaxeuse::cree_message_erreur(kuri::chaine_statique message)
+{
+	auto enchaineuse = Enchaineuse();
+	auto lexeme = lexeme_courant();
+
+	enchaineuse << "\n";
+	enchaineuse << m_fichier->chemin() << ':' << lexeme->ligne + 1 << " : erreur de syntaxage :\n";
+
+	POUR (m_donnees_etat_syntaxage) {
+		erreur::imprime_ligne_avec_message(enchaineuse, m_fichier, it.lexeme, it.message);
+	}
+
+	erreur::imprime_ligne_avec_message(enchaineuse, m_fichier, lexeme, message);
+	return enchaineuse.chaine();
+}
+
+void BaseSyntaxeuse::rapporte_erreur(kuri::chaine_statique message)
+{
+	m_possede_erreur = true;
+	gere_erreur_rapportee(cree_message_erreur(message));
+}
---
+++ logiciels/kuri/compilation/base_syntaxeuse.hh
@@ -0,0 +1,158 @@
+/*
+ * ***** BEGIN GPL LICENSE BLOCK *****
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software  Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The Original Code is Copyright (C) 2021 Kévin Dietrich.
+ * All rights reserved.
+ *
+ * ***** END GPL LICENSE BLOCK *****
+ *
+ */
+
+#pragma once
+
+#include "biblinternes/chrono/outils.hh"
+#include "biblinternes/structures/tablet.hh"
+
+#include "structures/chaine.hh"
+#include "structures/tableau.hh"
+
+#include "lexemes.hh"
+
+struct Fichier;
+
+struct BaseSyntaxeuse {
+protected:
+	/* Pour les messages d'erreurs. */
+	struct DonneesEtatSyntaxage {
+		Lexeme *lexeme = nullptr;
+		kuri::chaine_statique message{};
+	};
+
+	kuri::tableau<Lexeme, int> &m_lexemes;
+	Fichier *m_fichier = nullptr;
+	Lexeme *m_lexeme_courant = nullptr;
+	int m_position = 0;
+	bool m_possede_erreur = false;
+	char _pad[3];
+
+	dls::chrono::metre_seconde m_chrono_analyse{};
+
+	dls::tablet<DonneesEtatSyntaxage, 33> m_donnees_etat_syntaxage{};
+
+public:
+	BaseSyntaxeuse(Fichier *fichier);
+
+	COPIE_CONSTRUCT(BaseSyntaxeuse);
+
+	virtual ~BaseSyntaxeuse() = default;
+
+	void analyse();
+
+	bool possede_erreur() const
+	{
+		return m_possede_erreur;
+	}
+
+protected:
+	// Interface pour les classes dérivées.
+
+	// Cette fonction sera appelée à chaque itération de la boucle principale
+	// tant qu'il reste des léxèmes ou qu'il n'y a aucune erreur.
+	virtual void analyse_une_chose() = 0;
+
+	// Cette fonction optionnelle sera appelée avant de commencer la boucle principale.
+	virtual void quand_commence() {}
+
+	// Cette fonction optionnelle sera appelée à la fin de la boucle principale, ou s'il
+	// n'a pas de lexèmes dans la source
+	virtual void quand_termine() {}
+
+	// Cette fonction est appelée quand une erreur est rapportée via rapporte_erreur
+	virtual void gere_erreur_rapportee(kuri::chaine const &message_erreur) = 0;
+
+	// Interface pour la consommation et l'appariement de lexèmes
+
+	inline void consomme()
+	{
+		m_position += 1;
+
+		if (!fini()) {
+			m_lexeme_courant += 1;
+		}
+	}
+
+	inline void consomme(GenreLexeme genre_lexeme, kuri::chaine_statique message)
+	{
+		if (m_lexemes[m_position].genre != genre_lexeme) {
+			rapporte_erreur(message);
+			return;
+		}
+
+		return consomme();
+	}
+
+	inline void recule()
+	{
+		m_position -= 1;
+
+		if (m_position >= 0) {
+			m_lexeme_courant = &m_lexemes[m_position];
+		}
+	}
+
+	inline Lexeme *lexeme_courant()
+	{
+		return m_lexeme_courant;
+	}
+
+	inline Lexeme const *lexeme_courant() const
+	{
+		return m_lexeme_courant;
+	}
+
+	inline bool fini() const
+	{
+		return m_position >= m_lexemes.taille();
+	}
+
+	inline bool apparie(GenreLexeme genre_lexeme) const
+	{
+		return m_lexeme_courant->genre == genre_lexeme;
+	}
+
+	inline bool apparie(kuri::chaine_statique chaine) const
+	{
+		const auto chaine_lexeme = m_lexeme_courant->chaine;
+		const auto chaine_statique_lexeme = kuri::chaine_statique{ chaine_lexeme.pointeur(), chaine_lexeme.taille() };
+		return chaine_statique_lexeme == chaine;
+	}
+
+	inline bool apparie(IdentifiantCode *ident) const
+	{
+		return m_lexeme_courant->ident == ident;
+	}
+
+	// Interface pour la gestion d'erreurs
+
+	void empile_etat(kuri::chaine_statique message, Lexeme *lexeme);
+
+	void depile_etat();
+
+	kuri::chaine cree_message_erreur(kuri::chaine_statique message);
+
+	void rapporte_erreur(kuri::chaine_statique message);
+};
