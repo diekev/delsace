fb78c76a468474cabc6fc0e5a664ce4271e445b1
Kévin Dietrich <kevin.dietrich@mailoo.org>
2022-05-07 17:32:15 +0200

    dév(parcer) : cas spéciaux pour les structure de Géométrie3D

--- logiciels/kuri/executable/parcer.cc
+++ logiciels/kuri/executable/parcer.cc
@@ -1213,7 +1213,12 @@ struct Convertisseuse {
                 else {
                     imprime_commentaire(cursor, flux_sortie);
                     auto nom = determine_nom_anomyme(cursor, typedefs, nombre_anonymes);
-                    if (nom != "ContexteKuri") {
+                    // À FAIRE : paramétrise ceci
+                    if (nom == "AdaptriceMaillage" || nom == "Interruptrice" ||
+                        nom == "ContexteEvaluation") {
+                        flux_sortie << "importe Géométrie3D\n\n";
+                    }
+                    else if (nom != "ContexteKuri") {
                         imprime_tab(flux_sortie);
                         flux_sortie << nom;
                         flux_sortie << " :: struct #externe;\n\n";
