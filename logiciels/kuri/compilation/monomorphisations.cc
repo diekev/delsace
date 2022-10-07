24d50d819cce70913a85636d8dae001d60160a3b
Kévin Dietrich <kevin.dietrich@mailoo.org>
2022-10-07 19:44:15 +0200

    net(kuri) : utilise une structure de base pour Monomorphisations
    Ceci nous évite de générer trop de code, et nous permet de référencer
    génériquement les monomorphisations sans avoir de cas spéciaux dû à
    l'utilisation de gabarits.

--- logiciels/kuri/compilation/monomorphisations.cc
+++ logiciels/kuri/compilation/monomorphisations.cc
@@ -36,3 +36,85 @@ std::ostream &operator<<(std::ostream &os, const ItemMonomorphisation &item)
     }
     return os;
 }
+
+NoeudExpression *BaseMonorphisations::trouve_monomorphisation_impl(const tableau_items &items) const
+{
+    auto monomorphisations_ = monomorphisations.verrou_lecture();
+
+    POUR (*monomorphisations_) {
+        if (it.premier.taille() != items.taille()) {
+            continue;
+        }
+
+        auto trouve = true;
+
+        for (auto i = 0; i < items.taille(); ++i) {
+            if (it.premier[i] != items[i]) {
+                trouve = false;
+                break;
+            }
+        }
+
+        if (!trouve) {
+            continue;
+        }
+
+        return it.second;
+    }
+
+    return nullptr;
+}
+
+void BaseMonorphisations::ajoute(const tableau_items &items, NoeudExpression *noeud)
+{
+    monomorphisations->ajoute({items, noeud});
+}
+
+long BaseMonorphisations::memoire_utilisee() const
+{
+    long memoire = 0;
+    memoire += monomorphisations->taille() *
+            (taille_de(NoeudExpression *) + taille_de(tableau_items));
+
+    POUR (*monomorphisations.verrou_lecture()) {
+        memoire += it.premier.taille() * (taille_de(ItemMonomorphisation));
+    }
+
+    return memoire;
+}
+
+int BaseMonorphisations::taille() const
+{
+    return monomorphisations->taille();
+}
+
+int BaseMonorphisations::nombre_items_max() const
+{
+    int n = 0;
+
+    POUR (*monomorphisations.verrou_lecture()) {
+        if (it.premier.taille() > n) {
+            n = it.premier.taille();
+        }
+    }
+
+    return n;
+}
+
+void BaseMonorphisations::imprime(std::ostream &os)
+{
+    auto monomorphisations_ = monomorphisations.verrou_lecture();
+    if (monomorphisations_->taille() == 0) {
+        os << "Il n'y a aucune monomorphisation connue !\n";
+        return;
+    }
+
+    os << "Les monomophisations sont :\n";
+    POUR (*monomorphisations_) {
+        os << "-- :\n";
+
+        for (auto i = 0; i < it.premier.taille(); ++i) {
+            os << "  -- " << it.premier[i] << '\n';
+        }
+    }
+}
--- logiciels/kuri/compilation/monomorphisations.hh
+++ logiciels/kuri/compilation/monomorphisations.hh
@@ -71,95 +71,32 @@ struct ItemMonomorphisation {
 
 std::ostream &operator<<(std::ostream &os, const ItemMonomorphisation &item);
 
-template <typename TypeNoeud>
-struct Monomorphisations {
-  private:
+struct BaseMonorphisations {
+  protected:
     template <typename T>
     using tableau_synchrone = dls::outils::Synchrone<kuri::tableau<T, int>>;
 
     using tableau_items = kuri::tableau<ItemMonomorphisation, int>;
-    tableau_synchrone<dls::paire<tableau_items, TypeNoeud *>> monomorphisations{};
+    tableau_synchrone<dls::paire<tableau_items, NoeudExpression *>> monomorphisations{};
+
+    NoeudExpression *trouve_monomorphisation_impl(tableau_items const &items) const;
 
   public:
+    void ajoute(tableau_items const &items, NoeudExpression *noeud);
+
+    long memoire_utilisee() const;
+
+    int taille() const;
+
+    int nombre_items_max() const;
+
+    void imprime(std::ostream &os);
+};
+
+template <typename TypeNoeud>
+struct Monomorphisations : public BaseMonorphisations {
     TypeNoeud *trouve_monomorphisation(tableau_items const &items) const
     {
-        auto monomorphisations_ = monomorphisations.verrou_lecture();
-
-        POUR (*monomorphisations_) {
-            if (it.premier.taille() != items.taille()) {
-                continue;
-            }
-
-            auto trouve = true;
-
-            for (auto i = 0; i < items.taille(); ++i) {
-                if (it.premier[i] != items[i]) {
-                    trouve = false;
-                    break;
-                }
-            }
-
-            if (!trouve) {
-                continue;
-            }
-
-            return it.second;
-        }
-
-        return nullptr;
-    }
-
-    void ajoute(tableau_items const &items, TypeNoeud *noeud)
-    {
-        monomorphisations->ajoute({items, noeud});
-    }
-
-    long memoire_utilisee() const
-    {
-        long memoire = 0;
-        memoire += monomorphisations->taille() *
-                   (taille_de(TypeNoeud *) + taille_de(tableau_items));
-
-        POUR (*monomorphisations.verrou_lecture()) {
-            memoire += it.premier.taille() * (taille_de(ItemMonomorphisation));
-        }
-
-        return memoire;
-    }
-
-    int taille() const
-    {
-        return monomorphisations->taille();
-    }
-
-    int nombre_items_max() const
-    {
-        int n = 0;
-
-        POUR (*monomorphisations.verrou_lecture()) {
-            if (it.premier.taille() > n) {
-                n = it.premier.taille();
-            }
-        }
-
-        return n;
-    }
-
-    void imprime(std::ostream &os)
-    {
-        auto monomorphisations_ = monomorphisations.verrou_lecture();
-        if (monomorphisations_->taille() == 0) {
-            os << "Il n'y a aucune monomorphisation connue !\n";
-            return;
-        }
-
-        os << "Les monomophisations sont :\n";
-        POUR (*monomorphisations_) {
-            os << "-- :\n";
-
-            for (auto i = 0; i < it.premier.taille(); ++i) {
-                os << "  -- " << it.premier[i] << '\n';
-            }
-        }
+        return static_cast<TypeNoeud *>(trouve_monomorphisation_impl(items));
     }
 };
