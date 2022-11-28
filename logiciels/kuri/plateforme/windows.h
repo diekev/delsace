2ad6ddc021a591765ab3c5fc6c2b4f94be3e86d7
Kévin Dietrich <kevin.dietrich@mailoo.org>
2022-11-28 13:35:42 +0100

    Ajoute windows.h

---
+++ logiciels/kuri/plateforme/windows.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later
+ * The Original Code is Copyright (C) 2022 Kévin Dietrich. */
+
+// #pragma once
+
+/* MSVC définis pour ses extensions "interface" comme "struct". */
+
+#ifdef _MSC_VER
+#  undef interface
+#  undef OPAQUE
+#  undef DIFFERENCE
+#endif
