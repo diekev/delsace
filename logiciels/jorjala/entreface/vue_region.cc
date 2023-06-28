00de1d9089010e1b6b51dbb23f2e91ce59bc59f6
Kévin Dietrich <kevin.dietrich@mailoo.org>
2023-06-28 14:25:11 +0200

    fait(Jorjala) : permet d'ajouter et de supprimer des éditrices

--- logiciels/jorjala/entreface/vue_region.cc
+++ logiciels/jorjala/entreface/vue_region.cc
@@ -3,6 +3,19 @@
 
 #include "vue_region.hh"
 
+#if defined(__GNUC__)
+#    pragma GCC diagnostic push
+#    pragma GCC diagnostic ignored "-Wconversion"
+#    pragma GCC diagnostic ignored "-Wuseless-cast"
+#    pragma GCC diagnostic ignored "-Weffc++"
+#    pragma GCC diagnostic ignored "-Wsign-conversion"
+#endif
+#include <QMenu>
+#include <QPushButton>
+#if defined(__GNUC__)
+#    pragma GCC diagnostic pop
+#endif
+
 //#include "editrice_arborescence.hh"
 #include "editrice_ligne_temps.h"
 #include "editrice_noeud.h"
@@ -45,22 +58,62 @@ static BaseEditrice *qéditrice_depuis_éditrice(JJL::Jorjala &jorjala, JJL::Edi
 /** \} */
 
 /* ------------------------------------------------------------------------- */
+/** \name ActionAjoutEditrice.
+ * \{ */
+
+ActionAjoutEditrice::ActionAjoutEditrice(QString texte) : QAction(texte)
+{
+    connect(this, &QAction::triggered, this, &ActionAjoutEditrice::sur_declenchage);
+}
+
+void ActionAjoutEditrice::sur_declenchage()
+{
+    Q_EMIT(ajoute_editrice(this->data().toInt()));
+}
+
+/** \} */
+
+/* ------------------------------------------------------------------------- */
 /** \name VueRegion
  * \{ */
 
 VueRegion::VueRegion(JJL::Jorjala &jorjala, JJL::RegionInterface région, QWidget *parent)
-    : QTabWidget(parent), m_région(région)
+    : QTabWidget(parent), m_jorjala(jorjala), m_région(région),
+      m_bouton_affichage_liste(new QPushButton("Ajouter Éditrice", this)),
+      m_menu_liste_éditrices(new QMenu(this))
 {
     for (auto éditrice : m_région.éditrices()) {
-        auto qéditrice = qéditrice_depuis_éditrice(jorjala, éditrice);
-        if (!qéditrice) {
-            continue;
-        }
-
-        addTab(qéditrice, éditrice.nom().vers_std_string().c_str());
+        ajoute_page_pour_éditrice(éditrice, false);
     }
 
+    connect(m_bouton_affichage_liste, &QPushButton::clicked, this, &VueRegion::montre_liste);
+
+    setTabsClosable(true);
+    setCornerWidget(m_bouton_affichage_liste);
+
     connect(this, &QTabWidget::currentChanged, this, &VueRegion::ajourne_pour_changement_page);
+    connect(this, &QTabWidget::tabCloseRequested, this, &VueRegion::sur_fermeture_page);
+
+    /* Construit la liste d'éditrices. */
+    ActionAjoutEditrice *action;
+
+#define AJOUTE_ACTION(type_jorjala)                                                               \
+    action = new ActionAjoutEditrice(                                                             \
+        JJL::nom_pour_type_éditrice(type_jorjala).vers_std_string().c_str());                     \
+    m_menu_liste_éditrices->addAction(action);                                                    \
+    action->setData(QVariant(int(type_jorjala)));                                                 \
+    connect(action, &ActionAjoutEditrice::ajoute_editrice, this, &VueRegion::sur_ajout_editrice);
+
+    // AJOUTE_ACTION(JJL::TypeEditrice::ARBORESCENCE)
+    AJOUTE_ACTION(JJL::TypeEditrice::ATTRIBUTS)
+    AJOUTE_ACTION(JJL::TypeEditrice::GRAPHE)
+    AJOUTE_ACTION(JJL::TypeEditrice::LIGNE_TEMPS)
+    AJOUTE_ACTION(JJL::TypeEditrice::PROPRIÉTÉS_NOEUDS)
+    // AJOUTE_ACTION(JJL::TypeEditrice::RENDU)
+    AJOUTE_ACTION(JJL::TypeEditrice::VUE_2D)
+    AJOUTE_ACTION(JJL::TypeEditrice::VUE_3D)
+
+#undef AJOUTE_ACTION
 }
 
 void VueRegion::ajourne_éditrice_active(JJL::TypeEvenement évènement)
@@ -73,9 +126,51 @@ void VueRegion::ajourne_éditrice_active(JJL::TypeEvenement évènement)
     éditrice->ajourne_état(évènement);
 }
 
+void VueRegion::ajoute_page_pour_éditrice(JJL::Editrice éditrice, bool définit_comme_page_courante)
+{
+    auto qéditrice = qéditrice_depuis_éditrice(m_jorjala, éditrice);
+    if (!qéditrice) {
+        return;
+    }
+
+    addTab(qéditrice, éditrice.nom().vers_std_string().c_str());
+
+    if (définit_comme_page_courante) {
+        setCurrentIndex(count() - 1);
+    }
+}
+
 void VueRegion::ajourne_pour_changement_page(int /*index*/)
 {
     ajourne_éditrice_active(JJL::TypeEvenement::RAFRAICHISSEMENT);
+}
+
+void VueRegion::sur_fermeture_page(int index)
+{
+    auto éditrice = dynamic_cast<BaseEditrice *>(widget(index));
+    if (!éditrice) {
+        return;
+    }
+
+    m_région.supprime_éditrice_à_l_index(index);
+    removeTab(index);
+}
+
+void VueRegion::montre_liste()
+{
+    /* La liste est positionnée en dessous du bouton, alignée à sa gauche. */
+    const auto &rect = m_bouton_affichage_liste->geometry();
+    const auto &bas_gauche = m_bouton_affichage_liste->parentWidget()->mapToGlobal(
+        rect.bottomLeft());
+
+    m_menu_liste_éditrices->popup(bas_gauche);
+}
+
+void VueRegion::sur_ajout_editrice(int type)
+{
+    auto type_éditrice = static_cast<JJL::TypeEditrice>(type);
+    auto éditrice = m_région.ajoute_une_éditrice(type_éditrice);
+    ajoute_page_pour_éditrice(éditrice, true);
 }
 
 /** \} */
--- logiciels/jorjala/entreface/vue_region.hh
+++ logiciels/jorjala/entreface/vue_region.hh
@@ -10,12 +10,35 @@
 #    pragma GCC diagnostic ignored "-Weffc++"
 #    pragma GCC diagnostic ignored "-Wsign-conversion"
 #endif
+#include <QAction>
 #include <QTabWidget>
 #if defined(__GNUC__)
 #    pragma GCC diagnostic pop
 #endif
 
 #include "coeur/jorjala.hh"
+
+class QMenu;
+class QPushButton;
+
+/* ------------------------------------------------------------------------- */
+/** \name Sous-classe de QAction afin de controler les signaux.
+ * \{ */
+
+class ActionAjoutEditrice final : public QAction {
+    Q_OBJECT
+
+  public:
+    ActionAjoutEditrice(QString texte);
+
+  private Q_SLOTS:
+    void sur_declenchage();
+
+  Q_SIGNALS:
+    void ajoute_editrice(int type);
+};
+
+/** \} */
 
 /* ------------------------------------------------------------------------- */
 /** \name VueRegion
@@ -25,7 +48,10 @@
 class VueRegion final : public QTabWidget {
     Q_OBJECT
 
+    JJL::Jorjala &m_jorjala;
     JJL::RegionInterface m_région;
+    QPushButton *m_bouton_affichage_liste = nullptr;
+    QMenu *m_menu_liste_éditrices = nullptr;
 
   public:
     VueRegion(JJL::Jorjala &jorjala, JJL::RegionInterface région, QWidget *parent = nullptr);
@@ -33,8 +59,14 @@ class VueRegion final : public QTabWidget {
     /** Transmet l'évènement à l'éditrice courante. */
     void ajourne_éditrice_active(JJL::TypeEvenement évènement);
 
+  private:
+    void ajoute_page_pour_éditrice(JJL::Editrice éditrice, bool définit_comme_page_courante);
+
   private Q_SLOTS:
     void ajourne_pour_changement_page(int index);
+    void sur_fermeture_page(int index);
+    void montre_liste();
+    void sur_ajout_editrice(int type);
 };
 
 /** \} */
