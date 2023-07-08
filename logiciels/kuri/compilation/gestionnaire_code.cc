e194631b96989b6f53f1d4ee5b18d998dade5fe7
Kévin Dietrich <kevin.dietrich@mailoo.org>
2023-07-08 04:56:21 +0200

    Changement inutile

--- logiciels/kuri/compilation/gestionnaire_code.cc
+++ logiciels/kuri/compilation/gestionnaire_code.cc
@@ -1,4 +1,4 @@
-﻿/* SPDX-License-Identifier: GPL-2.0-or-later
+/* SPDX-License-Identifier: GPL-2.0-or-later
  * The Original Code is Copyright (C) 2021 Kévin Dietrich. */
 
 #include "gestionnaire_code.hh"
