706dbe18eb32b2465af03283ac6ed97ce41d7cbe
Kévin Dietrich <kevin.dietrich@mailoo.org>
2023-07-12 08:58:56 +0200

    dév(kanba) : feuille de style

--- logiciels/kanba/lanceur/lanceur.cc
+++ logiciels/kanba/lanceur/lanceur.cc
@@ -31,7 +31,47 @@
 #include <QFile>
 #pragma GCC diagnostic pop
 
+#include <optional>
+
 #include "entreface/fenetre_principale.h"
+
+struct OptionThème {
+    QString clé{};
+    QString valeur{};
+};
+
+static const OptionThème options_theme[] = {
+    {"$COULEUR_ARRIERE_PLAN_WIDGET$", "rgb(127, 127, 127)"},
+    {"$COULEUR_ARRIERE_PLAN_CONTROLE$", "rgb(40, 40, 40)"},
+    {"$COULEUR_SPLITTER$", "rgb(105, 105, 105)"},
+    {"$COULEUR_TEXTE$", "rgb(240, 240, 240)"},
+    {"$COULEUR_BORDURE$", "rgb(164, 164, 164)"},
+    {"$COULEUR_BORDURE_EDITRICE_ACTIVE$", "#52b1ee"},
+    {"$MARGE_CONTROLE$", "5px"},
+    /* Bouton (QPushButton). */
+    {"$COULEUR_BOUTON$", "rgb(150, 150, 150)"},
+    {"$COULEUR_BOUTON_PRESSE$", "rgb(90, 90, 90)"},
+    {"$COULEUR_BOUTON_SURVOL$", "rgb(160, 160, 160)"},
+    {"$RAYON_BORDURE_BOUTON$", "5px"},
+    {"$TAILLE_SPLITTER$", "3px"},
+};
+
+static std::optional<QString> donne_feuille_de_style()
+{
+    QFile file("styles/main.qss");
+    if (!file.open(QFile::ReadOnly)) {
+        return {};
+    }
+
+    QString feuille_de_style = file.readAll();
+    file.close();
+
+    for (auto const &option_theme : options_theme) {
+        feuille_de_style.replace(option_theme.clé, option_theme.valeur);
+    }
+
+    return feuille_de_style;
+}
 
 int main(int argc, char *argv[])
 {
@@ -39,11 +79,10 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("giraffeenfeu");
     QCoreApplication::setApplicationName("kanba");
 
-    QFile file("styles/main.qss");
-    file.open(QFile::ReadOnly);
-    QString style_sheet = QLatin1String(file.readAll());
-
-    qApp->setStyleSheet(style_sheet);
+    const auto opt_feuille_de_style = donne_feuille_de_style();
+    if (opt_feuille_de_style.has_value()) {
+        qApp->setStyleSheet(opt_feuille_de_style.value());
+    }
 
     FenetrePrincipale w;
     w.setWindowTitle(QCoreApplication::applicationName());
