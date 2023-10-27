e51d2ce30e25f23a6ceff747fef0163b7de1ca99
Kévin Dietrich <kevin.dietrich@mailoo.org>
2023-10-27 15:51:02 +0200

    net(kuri) : ajout d'un identifiant pour libc

--- logiciels/kuri/parsage/identifiant.hh
+++ logiciels/kuri/parsage/identifiant.hh
@@ -140,7 +140,8 @@ struct TableIdentifiant {
     ENUMERE_IDENTIFIANT_COMMUN_SIMPLE(chemin_de_ce_fichier, "chemin_de_ce_fichier")               \
     ENUMERE_IDENTIFIANT_COMMUN_SIMPLE(chemin_de_ce_module, "chemin_de_ce_module")                 \
     ENUMERE_IDENTIFIANT_COMMUN_SIMPLE(nom_de_cette_fonction, "nom_de_cette_fonction")             \
-    ENUMERE_IDENTIFIANT_COMMUN_SIMPLE(__ret0, "__ret0")
+    ENUMERE_IDENTIFIANT_COMMUN_SIMPLE(__ret0, "__ret0")                                           \
+    ENUMERE_IDENTIFIANT_COMMUN_SIMPLE(libc, "libc")
 
 namespace ID {
 #define ENUMERE_IDENTIFIANT_COMMUN_SIMPLE(x, y) extern IdentifiantCode *x;
