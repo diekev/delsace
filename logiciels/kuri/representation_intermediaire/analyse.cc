d106e8622373d13246f92bd69b209f5bd8d2fcc3
Kévin Dietrich <kevin.dietrich@mailoo.org>
2023-11-15 06:20:26 +0100

    dév(kuri) : rapporte un avertissement pour le code mort

--- logiciels/kuri/representation_intermediaire/analyse.cc
+++ logiciels/kuri/representation_intermediaire/analyse.cc
@@ -466,8 +466,25 @@ static bool détecte_blocs_invalides(EspaceDeTravail &espace,
             return false;
         }
 
-        auto di = it->instructions.dernière();
+        /* Nous pouvons avoir du code après un retour, par exemple le code après une discrimination
+         * dont toutes les branches retournent de la fonction.
+         * À FAIRE : déplace le diagnostique et ne génère pas la RI dans ce cas. */
+        auto branche_ou_retour_rencontré = false;
+        auto avertissement_donné = false;
+        POUR_NOMME (inst, it->instructions) {
+            if (inst->est_branche_ou_retourne()) {
+                branche_ou_retour_rencontré = true;
+                continue;
+            }
 
+            if (branche_ou_retour_rencontré && inst->site && !avertissement_donné) {
+                espace.rapporte_avertissement(inst->site,
+                                              "L'expression ne sera jamais évaluée.\n");
+                avertissement_donné = true;
+            }
+        }
+
+        auto di = it->instructions.dernière();
         if (di->est_branche_ou_retourne()) {
             continue;
         }
