f9e19a483f22f2f6ed9bc294862a2d459fb0bcd6
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-01-18 23:45:55 +0100

    dév(kuri) : impression textuelle des GenreValeurs

--- logiciels/kuri/arbre_syntaxique/utilitaires.cc
+++ logiciels/kuri/arbre_syntaxique/utilitaires.cc
@@ -137,6 +137,40 @@ std::ostream &operator<<(std::ostream &os, DrapeauxNoeudFonction const drapeaux)
 /** \} */
 
 /* ------------------------------------------------------------------------- */
+/** \name GenreValeur
+ * \{ */
+
+std::ostream &operator<<(std::ostream &os, GenreValeur const genre_valeur)
+{
+    if (genre_valeur == GenreValeur::INVALIDE) {
+        os << "INVALIDE";
+        return os;
+    }
+
+#define SI_DRAPEAU_UTILISE(drapeau)                                                               \
+    if ((genre_valeur & GenreValeur::drapeau) != GenreValeur(0)) {                                \
+        identifiants.ajoute(#drapeau);                                                            \
+    }
+
+    kuri::tablet<kuri::chaine_statique, 32> identifiants;
+
+    SI_DRAPEAU_UTILISE(GAUCHE)
+    SI_DRAPEAU_UTILISE(DROITE)
+
+    auto virgule = "";
+
+    POUR (identifiants) {
+        os << virgule << it;
+        virgule = " | ";
+    }
+
+#undef SI_DRAPEAU_UTILISE
+    return os;
+}
+
+/** \} */
+
+/* ------------------------------------------------------------------------- */
 /** \name Visibilité symbole.
  * \{ */
 
