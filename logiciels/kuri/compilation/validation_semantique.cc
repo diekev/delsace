4fa3006668c4659440610dd0b16a01fca554b34d
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-03-29 09:11:22 +0100

    cor(kuri) : aucune erreur pour la redéfinition des structs #externe sans bloc

--- logiciels/kuri/compilation/validation_semantique.cc
+++ logiciels/kuri/compilation/validation_semantique.cc
@@ -3868,6 +3868,18 @@ RésultatValidation Sémanticienne::valide_structure(NoeudStruct *decl)
         decl->type = m_compilatrice.typeuse.type_type_de_donnees(decl);
     }
 
+    if (!decl->est_monomorphisation) {
+        auto decl_precedente = trouve_dans_bloc(
+            decl->bloc_parent, decl, decl->bloc_parent->bloc_parent);
+
+        // la bibliothèque C a des symboles qui peuvent être les mêmes pour les fonctions et les
+        // structres (p.e. stat)
+        if (decl_precedente != nullptr && decl_precedente->genre == decl->genre) {
+            rapporte_erreur_redéfinition_symbole(decl, decl_precedente);
+            return CodeRetourValidation::Erreur;
+        }
+    }
+
     if (decl->est_externe && decl->bloc == nullptr) {
         decl->drapeaux |= DrapeauxNoeud::DECLARATION_FUT_VALIDEE;
         /* INITIALISATION_TYPE_FUT_CREEE est à cause de attente_sur_type_si_drapeau_manquant */
@@ -3924,18 +3936,6 @@ RésultatValidation Sémanticienne::valide_structure(NoeudStruct *decl)
     TENTE(valide_arbre_aplatis(decl));
 
     CHRONO_TYPAGE(m_stats_typage.structures, STRUCTURE__VALIDATION);
-
-    if (!decl->est_monomorphisation) {
-        auto decl_precedente = trouve_dans_bloc(
-            decl->bloc_parent, decl, decl->bloc_parent->bloc_parent);
-
-        // la bibliothèque C a des symboles qui peuvent être les mêmes pour les fonctions et les
-        // structres (p.e. stat)
-        if (decl_precedente != nullptr && decl_precedente->genre == decl->genre) {
-            rapporte_erreur_redéfinition_symbole(decl, decl_precedente);
-            return CodeRetourValidation::Erreur;
-        }
-    }
 
     auto type_compose = decl->comme_type_composé();
 
