968ea487e6fcf825df126c02fbd3689498c016dc
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-04-02 15:43:09 +0200

    dév(Qt) : ajout d'un rappel sur_destruction pour les widgets

--- logiciels/kuri/modules_c/Qt/qt_ipa_c.h
+++ logiciels/kuri/modules_c/Qt/qt_ipa_c.h
@@ -370,7 +370,8 @@ int QT_wheel_event_donne_delta(struct QT_WheelEvent *event);
     void (*sur_deplacement_souris)(struct type_classe *, struct QT_MouseEvent *);                 \
     void (*sur_relachement_souris)(struct type_classe *, struct QT_MouseEvent *);                 \
     void (*sur_molette_souris)(struct type_classe *, struct QT_WheelEvent *);                     \
-    void (*sur_redimensionnement)(struct type_classe *, struct QT_ResizeEvent *)
+    void (*sur_redimensionnement)(struct type_classe *, struct QT_ResizeEvent *);                 \
+    void (*sur_destruction)(struct type_classe *)
 
 struct QT_Rappels_Widget {
     RAPPELS_EVENEMENTS_COMMUNS(QT_Rappels_Widget);
