9ecd4b8d33a119d8d2a5ca167ba7f0e9deb4cf33
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-06-06 19:32:56 +0200

    dév(danjo) : apparente les controles au conteneur

--- logiciels/danjo/coeur/danjo/compilation/assembleuse_disposition.cc
+++ logiciels/danjo/coeur/danjo/compilation/assembleuse_disposition.cc
@@ -57,43 +57,45 @@
 
 namespace danjo {
 
-static ControlePropriete *cree_controle_pour_propriete(BasePropriete *propriete, int temps)
+static ControlePropriete *cree_controle_pour_propriete(BasePropriete *propriete,
+                                                       int temps,
+                                                       QWidget *parent)
 {
     switch (propriete->type()) {
         case TypePropriete::ENTIER:
-            return new ControleProprieteEntier(propriete, temps);
+            return new ControleProprieteEntier(propriete, temps, parent);
         case TypePropriete::DECIMAL:
-            return new ControleProprieteDecimal(propriete, temps);
+            return new ControleProprieteDecimal(propriete, temps, parent);
         case TypePropriete::ENUM:
-            return new ControleProprieteEnum(propriete, temps);
+            return new ControleProprieteEnum(propriete, temps, parent);
         case TypePropriete::LISTE:
-            return new ControleProprieteListe(propriete, temps);
+            return new ControleProprieteListe(propriete, temps, parent);
         case TypePropriete::BOOL:
-            return new ControleProprieteBool(propriete, temps);
+            return new ControleProprieteBool(propriete, temps, parent);
         case TypePropriete::CHAINE_CARACTERE:
-            return new ControleProprieteChaineCaractere(propriete, temps);
+            return new ControleProprieteChaineCaractere(propriete, temps, parent);
         case TypePropriete::FICHIER_ENTREE:
-            return new ControleProprieteFichier(propriete, temps);
+            return new ControleProprieteFichier(propriete, temps, parent);
         case TypePropriete::FICHIER_SORTIE:
-            return new ControleProprieteFichier(propriete, temps);
+            return new ControleProprieteFichier(propriete, temps, parent);
         case TypePropriete::DOSSIER:
-            return new ControleProprieteFichier(propriete, temps);
+            return new ControleProprieteFichier(propriete, temps, parent);
         case TypePropriete::COULEUR:
-            return new ControleProprieteCouleur(propriete, temps);
+            return new ControleProprieteCouleur(propriete, temps, parent);
         case TypePropriete::VECTEUR_DECIMAL:
-            return new ControleProprieteVecteurDecimal(propriete, temps);
+            return new ControleProprieteVecteurDecimal(propriete, temps, parent);
         case TypePropriete::VECTEUR_ENTIER:
-            return new ControleProprieteVecteurEntier(propriete, temps);
+            return new ControleProprieteVecteurEntier(propriete, temps, parent);
         case TypePropriete::COURBE_COULEUR:
-            return new ControleProprieteCourbeCouleur(propriete, temps);
+            return new ControleProprieteCourbeCouleur(propriete, temps, parent);
         case TypePropriete::COURBE_VALEUR:
-            return new ControleProprieteCourbeValeur(propriete, temps);
+            return new ControleProprieteCourbeValeur(propriete, temps, parent);
         case TypePropriete::RAMPE_COULEUR:
-            return new ControleProprieteRampeCouleur(propriete, temps);
+            return new ControleProprieteRampeCouleur(propriete, temps, parent);
         case TypePropriete::TEXTE:
-            return new ControleProprieteEditeurTexte(propriete, temps);
+            return new ControleProprieteEditeurTexte(propriete, temps, parent);
         case TypePropriete::LISTE_MANIP:
-            return new ControleProprieteListeManip(propriete, temps);
+            return new ControleProprieteListeManip(propriete, temps, parent);
         default:
             return nullptr;
     }
@@ -126,7 +128,7 @@ static ControlePropriete *crée_controle_propriété(DonneesControle const &donn
                                                   int temps,
                                                   ConteneurControles *conteneur)
 {
-    auto résultat = cree_controle_pour_propriete(prop, temps);
+    auto résultat = cree_controle_pour_propriete(prop, temps, conteneur);
     résultat->finalise(donnees);
 
     if (prop->type() == TypePropriete::LISTE) {
