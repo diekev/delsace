4cc0c0b22a5b4f05e15fdbfddcef90795508ca6f
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-07-22 00:46:48 +0200

    dév(JavaScript) : ajout de Array.prototype.push

--- logiciels/kuri/modules/JavaScript/Test/fichiers/array.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/array.js
@@ -2,6 +2,12 @@ var a = [1, 2, 3];
 
 // console.log(a);
 
+a[1] = 5;
+
+var push_result = a.push(7);
+
 for (var i = 0; i < a.length; i++) {
     console.log(a[i]);
 }
+
+console.log("résultat push : " + push_result);
