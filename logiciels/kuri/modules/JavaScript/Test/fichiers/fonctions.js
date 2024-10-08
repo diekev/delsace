adada295171f260c4f882a6e40af19bd99cdf538
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-08 12:17:00 +0200

    dév(JavaScript) : implémente SetFunctionLength

--- logiciels/kuri/modules/JavaScript/Test/fichiers/fonctions.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/fonctions.js
@@ -9,6 +9,9 @@ function accède_globale() {
     return (a + b) + c;
 }
 
+function avec_paramètres(a, b) {
+}
+
 test("fonction peut ne pas avoir de paramètres", function () {
     vérifie_égalité(sans_paramètres(), 6);
 });
@@ -20,4 +23,10 @@ test("une fonction peut accéder à une variable globale", function () {
 test("les fonctions doivent avoir une propriété 'name'", function () {
     vérifie_égalité(accède_globale.name, "accède_globale")
     vérifie_égalité(sans_paramètres.name, "sans_paramètres")
+});
+
+test("les fonctions doivent avoir une propriété 'length'", function () {
+    vérifie_égalité(accède_globale.length, 0)
+    vérifie_égalité(sans_paramètres.length, 0)
+    vérifie_égalité(avec_paramètres.length, 2)
 });
