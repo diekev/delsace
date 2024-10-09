130e4317ba0cf452286e83bd3751c0787ed5e651
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-09 07:59:10 +0200

    fait(JavaScript) : supporte "continue" et "break"

--- logiciels/kuri/modules/JavaScript/Test/fichiers/boucle-for.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/boucle-for.js
@@ -40,3 +40,31 @@ test("boucles for imbriquées sans bloc", function () {
 
     vérifie_égalité(résultat, 450);
 });
+
+test("boucle for avec continue", function () {
+    var résultat = 0;
+
+    for (var i = 0; i < 10; i += 1) {
+        if (i == 5) {
+            continue;
+        }
+
+        résultat += i;
+    }
+
+    vérifie_égalité(résultat, 40);
+});
+
+test("boucle for avec break", function () {
+    var résultat = 0;
+
+    for (var i = 0; i < 10; i += 1) {
+        if (i == 5) {
+            break;
+        }
+
+        résultat += i;
+    }
+
+    vérifie_égalité(résultat, 10);
+});
