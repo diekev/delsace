5f74f6bdbfc738d2153790857c6b831bc601f751
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-09 10:09:59 +0200

    dév(JavaScript) : supporte les "++" et "--" préfixés

--- logiciels/kuri/modules/JavaScript/Test/fichiers/expression-update.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/expression-update.js
@@ -10,3 +10,15 @@ test("postfix", function () {
     vérifie_égalité(w, 0);
     vérifie_égalité(z, 1);
 });
+
+test("pretfix", function () {
+    var x = 0;
+    var y = ++x;
+    vérifie_égalité(x, 1);
+    vérifie_égalité(y, 1);
+
+    var w = 1;
+    var z = --w;
+    vérifie_égalité(w, 0);
+    vérifie_égalité(z, 0);
+});
