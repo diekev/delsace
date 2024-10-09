f3c0064df687f0e604f78a6887bc6c7befe802ec
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-09 13:01:35 +0200

    dév(JavaScript) : lèxe les caractères échappés dans les chaines

--- logiciels/kuri/modules/JavaScript/Test/fichiers/lexage.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/lexage.js
@@ -4,3 +4,10 @@ test("les opérateurs sont proprement lexés même si les espaces sont manquants
     var b =a>=-1;
     vérifie_égalité(b, true);
 });
+
+test("les caractères échappées ne compte pas comme des caractères finaux", function() {
+    var s = "*([^\\]'\"]*?)";
+    var t = "\\";
+    var s = '*([^\\]\'"]*?)';
+    var t = '\\';
+});
