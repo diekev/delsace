8c35eb8cfeef8bbed1a075a0bcad22fc104c2c3a
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-09 11:26:06 +0200

    dév(JavaScript) : test pour le lexage

---
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/lexage.js
@@ -0,0 +1,6 @@
+test("les opérateurs sont proprement lexés même si les espaces sont manquants", function () {
+    var a=-1;
+    vérifie_égalité(a, -1);
+    var b =a>=-1;
+    vérifie_égalité(b, true);
+});
