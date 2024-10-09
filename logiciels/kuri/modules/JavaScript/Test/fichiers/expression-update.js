04b55ed7a6b21f357019fa6b1e26a57fa4a8a01e
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-09 10:02:45 +0200

    dév(JavaScript) : test pour les expressions d'acrément

---
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/expression-update.js
@@ -0,0 +1,12 @@
+
+test("postfix", function () {
+    var x = 0;
+    var y = x++;
+    vérifie_égalité(x, 1);
+    vérifie_égalité(y, 0);
+
+    var w = 1;
+    var z = w--;
+    vérifie_égalité(w, 0);
+    vérifie_égalité(z, 1);
+});
