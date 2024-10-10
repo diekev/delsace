08cdd4db7ba5e728e60006551f2d414c359d60e0
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-10 06:23:04 +0200

    dév(JavaScript) : ajout de tests

---
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/objet-object.js
@@ -0,0 +1,7 @@
+test("properties of the Object constructor", function () {
+    vérifie_égalité(Object.length, 1)
+});
+
+test("properties of the Object prototype", function () {
+    vérifie_égalité(Object.prototype.constructor, Object)
+});
