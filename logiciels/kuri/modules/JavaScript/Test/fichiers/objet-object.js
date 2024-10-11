06ac3928ee58c7bf93dcd9ec7e68f528af2bff86
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-11 17:20:41 +0200

    fait(JavaScript) : ajout de Object.prototype.__proto__

--- logiciels/kuri/modules/JavaScript/Test/fichiers/objet-object.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/objet-object.js
@@ -6,6 +6,11 @@ test("properties of the Object prototype", function () {
     vérifie_égalité(Object.prototype.constructor, Object)
 });
 
+test("Object.prototype.__proto__", function () {
+    var o = {};
+    vérifie_égalité(o.__proto__, Object.getPrototypeOf(o))
+});
+
 test("Object.getPrototypeOf", function () {
     var o = {};
     vérifie_égalité(Object.getPrototypeOf(o), Object.prototype)
