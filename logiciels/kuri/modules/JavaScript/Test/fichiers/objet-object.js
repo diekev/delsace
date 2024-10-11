4a28867f2a5688591c34af6082f2e09fd09dc0da
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-11 17:12:18 +0200

    fait(JavaScript) : ajout de Object.getPrototypeOf

--- logiciels/kuri/modules/JavaScript/Test/fichiers/objet-object.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/objet-object.js
@@ -5,3 +5,8 @@ test("properties of the Object constructor", function () {
 test("properties of the Object prototype", function () {
     vérifie_égalité(Object.prototype.constructor, Object)
 });
+
+test("Object.getPrototypeOf", function () {
+    var o = {};
+    vérifie_égalité(Object.getPrototypeOf(o), Object.prototype)
+});
