18943a42ae02c7cf0b9d0ea9ac8725954732e763
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-13 09:56:08 +0200

    dév(JavaScript) : ajout de Math.floor et Math.ceil

--- logiciels/kuri/modules/JavaScript/Test/fichiers/objet-math.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/objet-math.js
@@ -1,3 +1,26 @@
+test("Math.ceil", function () {
+    var x = Math.ceil(1.5);
+    vérifie_égalité(x, 2.0);
+
+    x = Math.ceil(-1.5);
+    vérifie_égalité(x, -1.0);
+
+    var fx = Math.floor(-1.5);
+    vérifie_égalité(Math.ceil(1.5), -fx);
+});
+
+test("Math.floor", function () {
+    var x = Math.floor(1.5);
+    vérifie_égalité(x, 1.0);
+
+    x = Math.floor(-1.5);
+    vérifie_égalité(x, -2.0);
+
+    /* À FAIRE : -Math.ceil(x) fait crasher. */
+    var cx = Math.ceil(-1.5);
+    vérifie_égalité(Math.floor(1.5), -cx);
+});
+
 test("Math.max", function () {
     var x = Math.max(1.0, 5.0, 2.0);
     vérifie_égalité(x, 5.0);
