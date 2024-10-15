8fed76049e0e3c7f4c0247f1a7ce027c3764d3ff
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-15 12:50:54 +0200

    fait(JavaScript) : implémente '^'

--- logiciels/kuri/modules/JavaScript/Test/fichiers/expression-binaire.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/expression-binaire.js
@@ -5,3 +5,12 @@ test("modulo", function () {
     vérifie_égalité(3 % 3, 0);
     vérifie_égalité(4 % 3, 1);
 });
+
+test("xor", function () {
+    vérifie_égalité(0 ^ 0, 0);
+    vérifie_égalité(0 ^ 1, 1);
+    vérifie_égalité(1 ^ 0, 1);
+    vérifie_égalité(1 ^ 1, 0);
+
+    vérifie_égalité(3 ^ 5, 6);
+});
