4bf758bd3614cfabb629d4ca806be52d3f9c765e
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-15 17:01:43 +0200

    dév(JavaScript) : ajout de String.prototype.indexOf

--- logiciels/kuri/modules/JavaScript/Test/fichiers/objet-string.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/objet-string.js
@@ -6,3 +6,15 @@ test("properties of the String constructor", function () {
 test("properties of the String prototype", function () {
     vérifie_égalité(String.prototype.constructor, String)
 });
+
+test("String.prototype.indexOf", function () {
+    var str = "Blue Whale";
+    vérifie_égalité(str.indexOf("Blue"), 0);
+    vérifie_égalité(str.indexOf("Blute"), -1);
+    vérifie_égalité(str.indexOf("Whale", 0), 5);
+    vérifie_égalité(str.indexOf("Whale", 5), 5);
+    vérifie_égalité(str.indexOf(""), 0);
+    vérifie_égalité(str.indexOf("", 9), 9);
+    vérifie_égalité(str.indexOf("", 10), 10);
+    vérifie_égalité(str.indexOf("", 11), 10);
+});
