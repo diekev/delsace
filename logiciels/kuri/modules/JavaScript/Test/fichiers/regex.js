1b37766117ed81834d5a841fc0e0786be3241eed
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-19 00:32:39 +0200

    dév(JavaScript) : cas supplémentaire pour traiter les / comme le début de regex

--- logiciels/kuri/modules/JavaScript/Test/fichiers/regex.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/regex.js
@@ -6,4 +6,9 @@ test("lexage des littérales regex", function () {
     var re5 = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
     var re6 = /^<(\w+)\s*\/?>(?:<\/\1>|)$/;
     var re7 = /:\/\/(www[0-9]?\.)?(.[^/:]+)/i;
+
+    function get_regex() {
+        return /^[\s\xa0]*$/;
+    }
+    var re8 = get_regex();
 })
