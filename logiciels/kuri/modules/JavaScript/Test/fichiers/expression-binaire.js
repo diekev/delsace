7baef973acc9200349bc1b1e77d48b9bbfd759f2
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-19 19:26:22 +0200

    dév(JavaScript) : tests pour xor

--- logiciels/kuri/modules/JavaScript/Test/fichiers/expression-binaire.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/expression-binaire.js
@@ -6,13 +6,23 @@ test("modulo", function () {
     vérifie_égalité(4 % 3, 1);
 });
 
-test("xor", function () {
+test("a ^ b", function () {
+    let a = 3;
+    let b = 5;
+    vérifie_égalité(a ^ b, 6);
+
     vérifie_égalité(0 ^ 0, 0);
     vérifie_égalité(0 ^ 1, 1);
     vérifie_égalité(1 ^ 0, 1);
     vérifie_égalité(1 ^ 1, 0);
+});
 
-    vérifie_égalité(3 ^ 5, 6);
+test("a ^= b a le même résultat que a ^ b", function () {
+    let a = 3;
+    let b = 5;
+    let c = a ^ b;
+    a ^= b
+    vérifie_égalité(a, c);
 });
 
 test("a | b", function () {
