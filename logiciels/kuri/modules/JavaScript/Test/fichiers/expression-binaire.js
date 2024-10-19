9985e3ee7dc422d7a40a5efc836f036a1bebdbde
Kévin Dietrich <kevin.dietrich@mailoo.org>
2024-10-19 19:25:00 +0200

    dév(JavaScript) : ajout de tests pour '|' et '|='

--- logiciels/kuri/modules/JavaScript/Test/fichiers/expression-binaire.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/expression-binaire.js
@@ -14,3 +14,22 @@ test("xor", function () {
 
     vérifie_égalité(3 ^ 5, 6);
 });
+
+test("a | b", function () {
+    let a = 3;
+    let b = 5;
+    vérifie_égalité(a | b, 7);
+
+    vérifie_égalité(0 | 0, 0);
+    vérifie_égalité(0 | 1, 1);
+    vérifie_égalité(1 | 0, 1);
+    vérifie_égalité(1 | 1, 1);
+});
+
+test("a |= b a le même résultat que a | b", function () {
+    let a = 3;
+    let b = 5;
+    let c = a | b;
+    a |= b
+    vérifie_égalité(a, c);
+});
