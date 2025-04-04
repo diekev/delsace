bc35b44ed24e0a1995b431f5fbf1da349e3ee6d7
Kévin Dietrich <kevin.dietrich@mailoo.org>
2025-04-04 08:54:48 +0200

    net(kuri) : formattage

--- logiciels/kuri/compilation/validation_expression_appel.cc
+++ logiciels/kuri/compilation/validation_expression_appel.cc
@@ -700,7 +700,8 @@ static RésultatAppariement apparie_construction_chaine(
     }
 
     if (args[0].expr->type != TypeBase::PTR_Z8) {
-        return ErreurAppariement::métypage_argument(args[0].expr, TypeBase::PTR_Z8, args[0].expr->type);
+        return ErreurAppariement::métypage_argument(
+            args[0].expr, TypeBase::PTR_Z8, args[0].expr->type);
     }
 
     auto résultat = cherche_transformation_pour_transtypage(args[1].expr->type, TypeBase::Z64);
@@ -710,7 +711,8 @@ static RésultatAppariement apparie_construction_chaine(
 
     auto transformation = std::get<TransformationType>(résultat);
     if (transformation.type == TypeTransformation::IMPOSSIBLE) {
-        return ErreurAppariement::métypage_argument(args[1].expr, TypeBase::Z64, args[1].expr->type);
+        return ErreurAppariement::métypage_argument(
+            args[1].expr, TypeBase::Z64, args[1].expr->type);
     }
 
     auto transformations = kuri::tableau<TransformationType, int>(2);
@@ -721,7 +723,8 @@ static RésultatAppariement apparie_construction_chaine(
     exprs.ajoute(args[0].expr);
     exprs.ajoute(args[1].expr);
 
-    return CandidateAppariement::construction_chaine(1.0, TypeBase::CHAINE, std::move(exprs), std::move(transformations));
+    return CandidateAppariement::construction_chaine(
+        1.0, TypeBase::CHAINE, std::move(exprs), std::move(transformations));
 }
 
 static RésultatAppariement apparie_appel_pointeur(
