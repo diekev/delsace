3784b2c528c51902f75a9d7fa91365ee5a2d1419
Kévin Dietrich <kevin.dietrich@mailoo.org>
2025-04-16 21:15:16 +0200

    dév(kuri) : suffixe les chaines constantes avec '\0'
    Ceci nous permets de mieux opérer avec C.

--- logiciels/kuri/compilation/coulisse_c.cc
+++ logiciels/kuri/compilation/coulisse_c.cc
@@ -2500,6 +2500,16 @@ void GénératriceCodeC::génère_code_pour_tableaux_données_constantes(
             os << dls::num::char_depuis_hex(octet & 0x0f);
             virgule = ", ";
         }
+
+        if (it.tableau->possède_drapeau(DrapeauxAtome::DONNÉES_CONSTANTES_SONT_POUR_CHAINE)) {
+            compteur++;
+            os << virgule;
+            if ((compteur % 20) == 0) {
+                os << "\n";
+            }
+            os << "0x00";
+            virgule = ", ";
+        }
     }
     os << "};\n";
 }
