bc76bc5f1ad0e8ff43462b7bd8d6c1c62e02fca8
Kévin Dietrich <kevin.dietrich@mailoo.org>
2025-08-10 23:14:27 +0200

    dév(JavaScript) : fonction async dans les littérales objets

--- logiciels/kuri/modules/JavaScript/Test/fichiers/async-function.js
+++ logiciels/kuri/modules/JavaScript/Test/fichiers/async-function.js
@@ -7,6 +7,15 @@ test("nous pouvons définir des méthodes async", function () {
     }
 });
 
+test("nous pouvons déclarer des fonction async dans littérales d'objets", function () {
+    var o = {
+        async fonction_attendu() { },
+        async fonction() {
+            const a = await this.fonction_attendu();
+        }
+    }
+});
+
 test("nous pouvons déclarer des fonction arrow asyn", function () {
     var e = async t => {
     }
