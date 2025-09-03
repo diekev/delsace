9ae7e58ff32fb0e02d419fbce57f2113fb2afede
Kévin Dietrich <kevin.dietrich@mailoo.org>
2025-09-03 15:20:24 +0200

    net(kuri) : formattage

--- biblinternes/outils/definitions.h
+++ biblinternes/outils/definitions.h
@@ -30,13 +30,13 @@
 #define CONCATENE(s1, s2) CONCATENE_IMPL(s1, s2)
 
 #ifndef __COUNTER__
-#	define VARIABLE_ANONYME(str) CONCATENE(str, __COUNTER__)
+#define VARIABLE_ANONYME(str) CONCATENE(str, __COUNTER__)
 #else
-#	define VARIABLE_ANONYME(str) CONCATENE(str, __LINE__)
+#define VARIABLE_ANONYME(str) CONCATENE(str, __LINE__)
 #endif
 
-#define ENLIGNE_TOUJOURS [[ gnu::always_inline ]]
-#define ENLIGNE_JAMAIS   [[ gnu::noinline ]]
+#define ENLIGNE_TOUJOURS [[gnu::always_inline]]
+#define ENLIGNE_JAMAIS [[gnu::noinline]]
 
 #define INUTILISE(x) static_cast<void>(x)
 
@@ -50,85 +50,76 @@
 #define IMPROBABLE(x) (__builtin_expect((x), 0))
 
 #if defined(__clang__) || defined(__GNUC__)
-#    define REMBOURRE(x) \
-        _Pragma("clang diagnostic push") \
-        _Pragma("clang diagnostic ignored \"-Wunused-private-field\"")  \
-        char VARIABLE_ANONYME(_pad)[x] \
-        _Pragma("clang diagnostic pop")
+#define REMBOURRE(x)                                                           \
+  _Pragma("clang diagnostic push")                                             \
+      _Pragma("clang diagnostic ignored \"-Wunused-private-field\"") char      \
+      VARIABLE_ANONYME(_pad)[x] _Pragma("clang diagnostic pop")
 #else
-#    define REMBOURRE(x)
+#define REMBOURRE(x)
 #endif
 
-#define COPIE_CONSTRUCT(x) \
-	x(x const &) = default; \
-	x &operator=(x const &) = default
+#define COPIE_CONSTRUCT(x)                                                     \
+  x(x const &) = default;                                                      \
+  x &operator=(x const &) = default
 
-#define EMPECHE_COPIE(x) \
-	x(x const &) = delete; \
-	x &operator=(x const &) = delete
+#define EMPECHE_COPIE(x)                                                       \
+  x(x const &) = delete;                                                       \
+  x &operator=(x const &) = delete
 
-#define COPIE_CONSTRUCT_MOUV(x) \
-	x(x &&) = default; \
-	x &operator=(x &&) = default
+#define COPIE_CONSTRUCT_MOUV(x)                                                \
+  x(x &&) = default;                                                           \
+  x &operator=(x &&) = default
 
-#define DEFINIS_OPERATEURS_DRAPEAU_IMPL(_type_drapeau_, _type_) \
-	inline constexpr auto operator&(_type_drapeau_ lhs, _type_drapeau_ rhs) \
-	{ \
-		return static_cast<_type_drapeau_>(static_cast<_type_>(lhs) & static_cast<_type_>(rhs)); \
-	} \
-	inline constexpr auto operator&(_type_drapeau_ lhs, _type_ rhs) \
-	{ \
-		return static_cast<_type_drapeau_>(static_cast<_type_>(lhs) & rhs); \
-	} \
-	inline constexpr auto operator|(_type_drapeau_ lhs, _type_drapeau_ rhs) \
-	{ \
-		return static_cast<_type_drapeau_>(static_cast<_type_>(lhs) | static_cast<_type_>(rhs)); \
-	} \
-	inline constexpr auto operator^(_type_drapeau_ lhs, _type_drapeau_ rhs) \
-	{ \
-		return static_cast<_type_drapeau_>(static_cast<_type_>(lhs) ^ static_cast<_type_>(rhs)); \
-	} \
-	inline constexpr auto operator~(_type_drapeau_ lhs) \
-	{ \
-		return static_cast<_type_drapeau_>(~static_cast<_type_>(lhs)); \
-	} \
-	inline constexpr auto &operator&=(_type_drapeau_ &lhs, _type_drapeau_ rhs) \
-	{ \
-		return (lhs = lhs & rhs); \
-	} \
-	inline constexpr auto &operator|=(_type_drapeau_ &lhs, _type_drapeau_ rhs) \
-	{ \
-		return (lhs = lhs | rhs); \
-	} \
-	inline constexpr auto &operator^=(_type_drapeau_ &lhs, _type_drapeau_ rhs) \
-	{ \
-		return (lhs = lhs ^ rhs); \
-    } \
-    inline bool drapeau_est_actif(_type_drapeau_ const v, _type_drapeau_ const d) \
-    { \
-        return (d & v) != _type_drapeau_(0); \
-    }
+#define DEFINIS_OPERATEURS_DRAPEAU_IMPL(_type_drapeau_, _type_)                \
+  inline constexpr auto operator&(_type_drapeau_ lhs, _type_drapeau_ rhs) {    \
+    return static_cast<_type_drapeau_>(static_cast<_type_>(lhs) &              \
+                                       static_cast<_type_>(rhs));              \
+  }                                                                            \
+  inline constexpr auto operator&(_type_drapeau_ lhs, _type_ rhs) {            \
+    return static_cast<_type_drapeau_>(static_cast<_type_>(lhs) & rhs);        \
+  }                                                                            \
+  inline constexpr auto operator|(_type_drapeau_ lhs, _type_drapeau_ rhs) {    \
+    return static_cast<_type_drapeau_>(static_cast<_type_>(lhs) |              \
+                                       static_cast<_type_>(rhs));              \
+  }                                                                            \
+  inline constexpr auto operator^(_type_drapeau_ lhs, _type_drapeau_ rhs) {    \
+    return static_cast<_type_drapeau_>(static_cast<_type_>(lhs) ^              \
+                                       static_cast<_type_>(rhs));              \
+  }                                                                            \
+  inline constexpr auto operator~(_type_drapeau_ lhs) {                        \
+    return static_cast<_type_drapeau_>(~static_cast<_type_>(lhs));             \
+  }                                                                            \
+  inline constexpr auto &operator&=(_type_drapeau_ &lhs, _type_drapeau_ rhs) { \
+    return (lhs = lhs & rhs);                                                  \
+  }                                                                            \
+  inline constexpr auto &operator|=(_type_drapeau_ &lhs, _type_drapeau_ rhs) { \
+    return (lhs = lhs | rhs);                                                  \
+  }                                                                            \
+  inline constexpr auto &operator^=(_type_drapeau_ &lhs, _type_drapeau_ rhs) { \
+    return (lhs = lhs ^ rhs);                                                  \
+  }                                                                            \
+  inline bool drapeau_est_actif(_type_drapeau_ const v,                        \
+                                _type_drapeau_ const d) {                      \
+    return (d & v) != _type_drapeau_(0);                                       \
+  }
 
-#define DEFINIS_OPERATEURS_DRAPEAU(_type_drapeau_) \
-    DEFINIS_OPERATEURS_DRAPEAU_IMPL(_type_drapeau_, std::underlying_type_t<_type_drapeau_>)
+#define DEFINIS_OPERATEURS_DRAPEAU(_type_drapeau_)                             \
+  DEFINIS_OPERATEURS_DRAPEAU_IMPL(_type_drapeau_,                              \
+                                  std::underlying_type_t<_type_drapeau_>)
 
 #define taille_de(x) static_cast<int64_t>(sizeof(x))
 
-#define POINTEUR_NUL(Type) \
-	static inline Type *nul() \
-	{ \
-		return static_cast<Type *>(nullptr); \
-	} \
-	static inline Type const *nul_const() \
-	{ \
-		return static_cast<Type const *>(nullptr); \
-	}
+#define POINTEUR_NUL(Type)                                                     \
+  static inline Type *nul() { return static_cast<Type *>(nullptr); }           \
+  static inline Type const *nul_const() {                                      \
+    return static_cast<Type const *>(nullptr);                                 \
+  }
 
 #define POUR(x) for (auto &it : (x))
 
 #define POUR_NOMME(nom, x) for (auto &nom : (x))
 
-#define POUR_INDEX(variable)                                                                      \
-    if (auto indice_it = -1)                                                                       \
-        for (auto &it : (variable))                                                               \
-            if (++indice_it, true)
+#define POUR_INDEX(variable)                                                   \
+  if (auto indice_it = -1)                                                     \
+    for (auto &it : (variable))                                                \
+      if (++indice_it, true)
