0f9a8fddb17564f763b901c4cedacef2d55eb05f
Kévin Dietrich <kevin.dietrich@mailoo.org>
2025-12-03 20:29:08 +0100

    dév(kuri) : conséquence d'une correction précédente pour les tuples

--- logiciels/kuri/representation_intermediaire/constructrice_ri.cc
+++ logiciels/kuri/representation_intermediaire/constructrice_ri.cc
@@ -3545,19 +3545,19 @@ void CompilatriceRI::transforme_valeur(NoeudExpression const *noeud,
         {
             valeur = crée_charge_mem_si_chargeable(noeud, valeur);
 
-            if (noeud->type->est_type_réel()) {
+            if (valeur->type->est_type_réel()) {
                 valeur = m_constructrice.crée_transtype(
                     noeud, transformation.type_cible, valeur, TypeTranstypage::DIMINUE_RÉEL);
             }
-            else if (noeud->type->est_type_entier_naturel()) {
+            else if (valeur->type->est_type_entier_naturel()) {
                 valeur = m_constructrice.crée_transtype(
                     noeud, transformation.type_cible, valeur, TypeTranstypage::DIMINUE_NATUREL);
             }
-            else if (noeud->type->est_type_entier_relatif()) {
+            else if (valeur->type->est_type_entier_relatif()) {
                 valeur = m_constructrice.crée_transtype(
                     noeud, transformation.type_cible, valeur, TypeTranstypage::DIMINUE_RELATIF);
             }
-            else if (noeud->type->est_type_bool()) {
+            else if (valeur->type->est_type_bool()) {
                 valeur = m_constructrice.crée_transtype(
                     noeud, transformation.type_cible, valeur, TypeTranstypage::DIMINUE_NATUREL);
             }
