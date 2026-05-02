1e9649bec86982524b221a3f8ced47a57e153f35
Kévin Dietrich <kevin.dietrich@mailoo.org>
2026-05-02 05:59:10 +0200

    dév(Qt) : expose QWindow.mapFromGlobal

--- logiciels/kuri/modules_c/Qt/qt_ipa_c.h
+++ logiciels/kuri/modules_c/Qt/qt_ipa_c.h
@@ -1565,6 +1565,9 @@ void QT_window_set_opacity(struct QT_Window *window, double opacity);
 void QT_window_map_to_global(struct QT_Window *window,
                              struct QT_Point *point,
                              struct QT_Point *r_point);
+void QT_window_map_from_global(struct QT_Window *window,
+                               struct QT_Point *point,
+                               struct QT_Point *r_point);
 void QT_window_set_mouse_grab_enabled(struct QT_Window *window, bool oui_non);
 void QT_window_set_keyboard_grab_enabled(struct QT_Window *window, bool oui_non);
 struct QT_Rect QT_window_geometry(struct QT_Window *window);
